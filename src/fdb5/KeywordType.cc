339d3b0f9c9daa9628df11d4ba978848ad2217da
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-04-13 14:16:42 +0100

    MARS-519: fdb5 refactor

--- src/fdb5/KeywordType.cc
+++ src/fdb5/KeywordType.cc
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2016 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -18,8 +18,6 @@
 #include "fdb5/KeywordType.h"
 #include "fdb5/DefaultKeywordHandler.h"
 
-#include "fdb5/Op.h"
-#include "fdb5/ForwardOp.h"
 
 using namespace eckit;
 
