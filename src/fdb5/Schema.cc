ecbed087b78045e371255f336c65ea540d9a339c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-04-23 17:36:31 +0100

    MARS-519: refactoring

---
+++ src/fdb5/Schema.cc
@@ -0,0 +1,121 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include <fstream>
+
+#include "eckit/log/Log.h"
+
+#include "marslib/MarsRequest.h"
+
+#include "fdb5/Schema.h"
+#include "fdb5/Rule.h"
+#include "fdb5/Key.h"
+#include "fdb5/SchemaParser.h"
+#include "fdb5/WriteVisitor.h"
+
+namespace fdb5 {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+Schema::Schema()
+{
+}
+
+Schema::~Schema()
+{
+    clear();
+}
+
+void Schema::expand(const MarsRequest& request, ReadVisitor& visitor) const
+{
+    Key full;
+    std::vector<Key> keys(3);
+
+    for(std::vector<Rule*>::const_iterator i = rules_.begin(); i != rules_.end(); ++i ) {
+        (*i)->expand(request, visitor, 0, keys, full);
+    }
+}
+
+void Schema::expand(const Key& field, WriteVisitor& visitor) const
+{
+    Key full;
+    std::vector<Key> keys(3);
+
+    visitor.rule(0); // reset to no rule so we verify that we pick at least one
+
+    for(std::vector<Rule*>::const_iterator i = rules_.begin(); i != rules_.end(); ++i ) {
+        (*i)->expand(field, visitor, 0, keys, full);
+    }
+}
+
+void Schema::load(const eckit::PathName& path, bool replace)
+{
+    if(replace) {
+        clear();
+    }
+
+    eckit::Log::info() << "Loading FDB rules from " << path << std::endl;
+
+    std::ifstream in(path.asString().c_str());
+    if(!in) {
+        throw eckit::CantOpenFile(path);
+    }
+
+    SchemaParser parser(in);
+
+    parser.parse(*this, rules_, handler_);
+
+    check();
+}
+
+void Schema::clear()
+{
+    for(std::vector<Rule*>::iterator i = rules_.begin(); i != rules_.end(); ++i ) {
+        delete *i;
+    }
+}
+
+void Schema::dump(std::ostream& s) const
+{
+    for(std::vector<Rule*>::const_iterator i = rules_.begin(); i != rules_.end(); ++i ) {
+        (*i)->dump(s);
+        s << std::endl;
+    }
+}
+
+void Schema::check()
+{
+    for(std::vector<Rule*>::iterator i = rules_.begin(); i != rules_.end(); ++i ) {
+        /// @todo print offending rule in meaningful message
+        ASSERT((*i)->depth() == 3);
+        (*i)->handlers_.updateParent(&handler_);
+        (*i)->updateParent(0);
+    }
+}
+
+void Schema::print(std::ostream& out) const
+{
+    out << "Schema()";
+}
+
+const KeywordHandler& Schema::lookupHandler(const std::string& keyword) const {
+    return handler_.lookupHandler(keyword);
+}
+
+
+std::ostream& operator<<(std::ostream& s, const Schema& x)
+{
+    x.print(s);
+    return s;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+} // namespace fdb5
---
+++ src/fdb5/Schema.h
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @file   Schema.h
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date   April 2016
+
+#ifndef fdb5_Schema_H
+#define fdb5_Schema_H
+
+#include <iosfwd>
+#include <vector>
+
+#include "eckit/memory/NonCopyable.h"
+#include "eckit/filesystem/PathName.h"
+#include "fdb5/Handlers.h"
+
+class MarsRequest;
+
+namespace fdb5 {
+
+class Key;
+class Rule;
+class ReadVisitor;
+class WriteVisitor;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class Schema : private eckit::NonCopyable {
+
+public: // methods
+
+    Schema();
+
+    ~Schema();
+
+    void expand(const Key& field, WriteVisitor& visitor) const;
+    void expand(const MarsRequest& request, ReadVisitor& visitor) const;
+
+    void load(const eckit::PathName& path, bool replace = false);
+
+    void dump(std::ostream& s) const;
+
+    const KeywordHandler& lookupHandler(const std::string& keyword) const;
+
+
+private: // methods
+
+    void clear();
+    void check();
+
+    friend std::ostream& operator<<(std::ostream& s,const Schema& x);
+
+    void print( std::ostream& out ) const;
+
+private: // members
+
+    Handlers handler_;
+    std::vector<Rule*>  rules_;
+
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+} // namespace fdb5
+
+#endif
