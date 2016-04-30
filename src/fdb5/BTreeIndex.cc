6f1654cb7fbb5b89c67bb0d7dfcc9b673d1b571e
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-04-30 15:46:03 +0100

    MARS-519: BTreeIndex class is back

---
+++ src/fdb5/BTreeIndex.cc
@@ -0,0 +1,78 @@
+/*
+ * (C) Copyright 1996-2013 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include "eckit/log/BigNum.h"
+#include "fdb5/BTreeIndex.h"
+#include "fdb5/TocIndex.h"
+
+namespace fdb5 {
+
+
+BTreeIndex::~BTreeIndex() {
+}
+
+//-----------------------------------------------------------------------------
+template<int KEYSIZE, int RECSIZE>
+TBTreeIndex<KEYSIZE, RECSIZE>::TBTreeIndex(const eckit::PathName &path, bool readOnly, off_t offset):
+    btree_( path, readOnly, offset) {
+}
+
+template<int KEYSIZE, int RECSIZE>
+bool TBTreeIndex<KEYSIZE, RECSIZE>::get(const std::string& key, FileStore::FieldRef &data) const {
+    BTreeKey k (key);
+    return btree_.get(k, data);
+}
+
+
+template<int KEYSIZE, int RECSIZE>
+bool TBTreeIndex<KEYSIZE, RECSIZE>::set(const std::string& key, const FileStore::FieldRef &data) {
+    BTreeKey k (key);
+    return btree_.set(k, data);
+}
+
+template<int KEYSIZE, int RECSIZE>
+void TBTreeIndex<KEYSIZE, RECSIZE>::flush() {
+    btree_.flush();
+}
+
+template<int KEYSIZE, int RECSIZE>
+
+class TBTreeIndexVisitor {
+    BTreeIndexVisitor &visitor_;
+public:
+    TBTreeIndexVisitor(BTreeIndexVisitor &visitor):
+        visitor_(visitor) {}
+
+    // BTree::range() expect a STL like collection
+
+    void clear() {
+    }
+
+    void push_back(const typename TBTreeIndex<KEYSIZE, RECSIZE>::BTreeStore::result_type &kv) {
+        visitor_.visit(kv.first, kv.second);
+    }
+};
+
+template<int KEYSIZE, int RECSIZE>
+void TBTreeIndex<KEYSIZE, RECSIZE>::visit(BTreeIndexVisitor &visitor) const {
+    TBTreeIndexVisitor<KEYSIZE, RECSIZE> v(visitor);
+    btree_.range("", "\255", v);
+}
+
+
+BTreeIndex_32_65536::BTreeIndex_32_65536(const eckit::PathName& path, bool readOnly, off_t offset):
+    TBTreeIndex<32, 65536>(path, readOnly, offset) {
+}
+
+
+
+//-----------------------------------------------------------------------------
+
+} // namespace fdb5
---
+++ src/fdb5/BTreeIndex.h
@@ -0,0 +1,84 @@
+/*
+ * (C) Copyright 1996-2013 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date Sep 2012
+
+#ifndef fdb5_BTreeIndex_H
+#define fdb5_BTreeIndex_H
+
+#include "eckit/eckit.h"
+
+#include "eckit/container/BTree.h"
+#include "eckit/io/Length.h"
+#include "eckit/io/Offset.h"
+#include "eckit/memory/NonCopyable.h"
+#include "eckit/memory/ScopedPtr.h"
+
+#include "eckit/types/FixedString.h"
+
+#include "fdb5/Index.h"
+
+namespace fdb5 {
+
+class BTreeIndexVisitor {
+public:
+    virtual void visit(const std::string& key, const FileStore::FieldRef&) = 0;
+};
+
+class BTreeIndex {
+public:
+    virtual ~ BTreeIndex();
+    virtual bool get(const std::string& key, FileStore::FieldRef& data) const = 0;
+    virtual bool set(const std::string& key, const FileStore::FieldRef& data)= 0;
+    virtual void flush() = 0;
+    virtual void visit(BTreeIndexVisitor& visitor) const = 0;
+
+};
+
+
+template<int KEYSIZE, int RECSIZE>
+class TBTreeIndex : public BTreeIndex {
+
+public: // types
+
+    typedef eckit::FixedString<KEYSIZE> BTreeKey;
+    typedef FileStore::FieldRef FieldRef;
+    typedef eckit::BTree< BTreeKey , FieldRef, RECSIZE > BTreeStore;
+
+public: // methods
+
+    TBTreeIndex(const eckit::PathName &path, bool readOnly, off_t offset );
+
+private: // methods
+
+    virtual bool get(const std::string& key, FileStore::FieldRef& data) const;
+    virtual bool set(const std::string& key, const FileStore::FieldRef& data);
+    virtual void flush();
+    virtual void visit(BTreeIndexVisitor& visitor) const;
+
+private: // members
+
+    mutable BTreeStore btree_;
+
+};
+
+
+
+
+class BTreeIndex_32_65536 : public TBTreeIndex<32, 65536> {
+public:
+    BTreeIndex_32_65536(const eckit::PathName& path, bool readOnly, off_t offset);
+};
+
+} // namespace fdb5
+
+#endif
