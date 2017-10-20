a6f8dfa8852ad08a0594ebbd95322e6fdb9042b3
Tiago Quintino <tiago.quintino@ecmwf.int>
2017-10-20 16:03:04 +0100

    MARS-590: add filters to fdb-write

--- src/fdb5/tools/fdb-write.cc
+++ src/fdb5/tools/fdb-write.cc
@@ -12,36 +12,58 @@
 #include "eckit/memory/ScopedPtr.h"
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/SimpleOption.h"
+#include "eckit/option/VectorOption.h"
 
 #include "fdb5/grib/GribArchiver.h"
 #include "fdb5/tools/FDBAccess.h"
 #include "fdb5/config/UMask.h"
 
 class FDBWrite : public fdb5::FDBAccess {
-    virtual void execute(const eckit::option::CmdArgs &args);
+
     virtual void usage(const std::string &tool) const;
+
+    virtual void init(const eckit::option::CmdArgs &args);
+
     virtual int minimumPositionalArguments() const { return 1; }
 
+    virtual void execute(const eckit::option::CmdArgs &args);
+
 public:
+
     FDBWrite(int argc, char **argv) : fdb5::FDBAccess(argc, argv) {
 
-//        options_.push_back(new eckit::option::SimpleOption<bool>("directory", "Override FDB5 root directory (only for TOC)"));
-//                           ->linkToResource("fdbRootDirectory"));
+        options_.push_back(
+                    new eckit::option::SimpleOption<std::string>("include-filter",
+                    "List of comma separated key-values of what to include from the input data"));
 
+        options_.push_back(
+                    new eckit::option::SimpleOption<std::string>("exclude-filter",
+                    "List of comma separated key-values of what to exclude from the input data"));
     }
+
+    std::string filterInclude_;
+    std::string filterExclude_;
 };
 
 void FDBWrite::usage(const std::string &tool) const {
-//    eckit::Log::info() << std::endl
-//                       << "Usage: " << tool << "[--directory=/path/to/root] gribfile1 [gribfile2] ..." << std::endl;
+    eckit::Log::info() << std::endl << "Usage: " << tool << " [--filter-include=...] [path1] [path2] ..." << std::endl;
     fdb5::FDBAccess::usage(tool);
+}
+
+void FDBWrite::init(const eckit::option::CmdArgs& args)
+{
+    args.get("include-filter", filterInclude_);
+    args.get("exclude-filter", filterExclude_);
 }
 
 void FDBWrite::execute(const eckit::option::CmdArgs &args) {
 
     fdb5::GribArchiver archiver;
 
+    archiver.filters(filterInclude_, filterExclude_);
+
     for (size_t i = 0; i < args.count(); i++) {
+
         eckit::PathName path(args(i));
 
         std::cout << "Processing " << path << std::endl;
