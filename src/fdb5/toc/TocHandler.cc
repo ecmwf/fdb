40e01f7d14170ceec9ef1890d2a978505238e477
Tiago Quintino <tiago.quintino@ecmwf.int>
2017-12-05 21:56:21 +0000

    MARS-691: Lustre API supported?

--- src/fdb5/toc/TocHandler.cc
+++ src/fdb5/toc/TocHandler.cc
@@ -911,9 +911,8 @@ std::string TocHandler::userName(long id) const {
 }
 
 bool TocHandler::stripeLustre() {
-
     static bool handleLustreStripe = eckit::Resource<bool>("fdbHandleLustreStripe;$FDB_HANDLE_LUSTRE_STRIPE", false);
-    return handleLustreStripe;
+    return fdb5LustreapiSupported() && handleLustreStripe;
 }
 
 
