f3b18a380cab398237f423420e86b50119ec107c
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-02-18 16:25:19 +0000

    MARS-707: remove need to update licenses every year

--- src/fdb5/toc/TocHandler.cc
+++ src/fdb5/toc/TocHandler.cc
@@ -1,5 +1,5 @@
 /*
- i (C) Copyright 1996-2017 ECMWF.
+ i (C) Copyright 1996- ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
