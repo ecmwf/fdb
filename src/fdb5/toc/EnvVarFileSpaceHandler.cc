b3611203e6da87d87db0c9ba7d0090675ddcd906
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-11-08 17:07:05 +0000

    MARS-753: FDB handler

---
+++ src/fdb5/toc/EnvVarFileSpaceHandler.cc
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include <sys/file.h>
+
+#include "fdb5/toc/EnvVarFileSpaceHandler.h"
+
+#include "eckit/config/Resource.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/filesystem/PathName.h"
+#include "eckit/thread/AutoLock.h"
+#include "eckit/io/FileLock.h"
+
+#include "fdb5/toc/FileSpace.h"
+#include "fdb5/database/Key.h"
+#include "fdb5/LibFdb.h"
+
+using namespace eckit;
+
+namespace fdb5 {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+EnvVarFileSpaceHandler::EnvVarFileSpaceHandler() :
+    fdbFileSpaceSHandlerEnvVarName_(LibResource<std::string, LibFdb>("fdbFileSpaceSHandlerEnvVarName;$FDB_FILESPACEHANDLER_ENVVARNAME", "SCHOST"))
+{
+}
+
+EnvVarFileSpaceHandler::~EnvVarFileSpaceHandler() {
+}
+
+
+PathName EnvVarFileSpaceHandler::select(const Key& key, const FileSpace& fs) const
+{
+    return FileSpaceHandler::lookup("WeightedRandom").selectFileSystem(key, fs);
+}
+
+eckit::PathName EnvVarFileSpaceHandler::selectFileSystem(const Key& key, const FileSpace& fs) const {
+
+    AutoLock<Mutex> lock(mutex_);
+
+    Log::debug<LibFdb>() << "Selecting a file system based on environment variable " << fdbFileSpaceSHandlerEnvVarName_ << std::endl;
+
+    const char* value  = ::getenv(fdbFileSpaceSHandlerEnvVarName_.c_str());
+    if(value) {
+        eckit::PathName path(value);
+
+        if(path.exists()) return path;
+
+        std::ostringstream msg;
+        msg << "";
+        throw ReadError(msg.str(), Here());
+    }
+    else {
+        std::ostringstream msg;
+        msg << "";
+        throw UserError(msg.str(), Here());
+    }
+}
+
+static FileSpaceHandlerRegister<EnvVarFileSpaceHandler> envvar("envvar");
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace fdb5
