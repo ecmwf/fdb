14e43f1f38b078a8f80c3dfde94058eb6082b3bd
Tiago Quintino <tiago.quintino@ecmwf.int>
2019-06-16 22:31:00 +0200

    FDB-158: reduce verbosity of Lustre API library

--- src/fdb5/config/UMask.cc
+++ src/fdb5/config/UMask.cc
@@ -19,11 +19,11 @@ namespace fdb5 {
 //----------------------------------------------------------------------------------------------------------------------
 
 UMask::UMask(mode_t mode):
-    save_(umask(mode)) {
+    save_(::umask(mode)) {
 }
 
 UMask::~UMask() {
-    umask(save_);
+    ::umask(save_);
 }
 
 mode_t UMask::defaultUMask() {
--- src/fdb5/io/LustreFileHandle.cc
+++ src/fdb5/io/LustreFileHandle.cc
@@ -14,15 +14,21 @@
 #include "eckit/exception/Exceptions.h"
 
 #include "fdb5/LibFdb5.h"
+#include "fdb5/io/LustreFileHandle.h"
 
 
 #if defined(HAVE_LUSTRE)
 extern "C" {
-int fdb5_lustreapi_file_create(const char* path, size_t stripesize, size_t stripecount);
+void fdb5_lustreapi_silence_msg();
+int  fdb5_lustreapi_file_create(const char* path, size_t stripesize, size_t stripecount);
 }
 #endif
 
+//----------------------------------------------------------------------------------------------------------------------
+
 namespace fdb5 {
+
+static bool lustreapi_silence = false;
 
 bool fdb5LustreapiSupported() {
 #if defined(HAVE_LUSTRE)
@@ -35,7 +41,14 @@ bool fdb5LustreapiSupported() {
 int fdb5LustreapiFileCreate(const char* path, size_t stripesize, size_t stripecount) {
 
 #if defined(HAVE_LUSTRE)
+
+    if(not lustreapi_silence) {
+        fdb5_lustreapi_silence_msg();
+        lustreapi_silence = true;
+    }
+
     return fdb5_lustreapi_file_create(path, stripesize, stripecount);
+
 #endif
 
     /// @note since fdb5LustreapiSupported() should be guarding all calls to this function,
@@ -45,5 +58,7 @@ int fdb5LustreapiFileCreate(const char* path, size_t stripesize, size_t stripeco
 
     return 0; /* should never be executed */
 }
+
+//----------------------------------------------------------------------------------------------------------------------
 
 } // namespace fdb5
