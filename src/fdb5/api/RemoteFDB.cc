7e95862ee9ed0e6db4cdfadd38e7e263abba5390
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2021-03-01 14:15:09 +0000

    implement canSeek in all DataHandle subclasses

--- src/fdb5/api/RemoteFDB.cc
+++ src/fdb5/api/RemoteFDB.cc
@@ -944,6 +944,7 @@ public: // methods
         overallPosition_(0),
         currentBuffer_(0),
         complete_(false) {}
+    virtual bool canSeek() const override { return false; }
 
 private: // methods
 
