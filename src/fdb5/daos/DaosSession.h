4a25bfb8d2252310e2431937906cd0a4e0187678
Nicolau Manubens <nicolau.manubens@ecmwf.int>
2023-01-26 16:13:50 +0100

    Revert "Some ideas"
    This reverts commit 391ce2bf8e7a4bfdc77fe645235209149aa828c9.

--- src/fdb5/daos/DaosSession.h
+++ src/fdb5/daos/DaosSession.h
@@ -19,57 +19,29 @@
 #include <deque>
 
 #include "eckit/exception/Exceptions.h"
-#include "fdb5/config/LocalConfiguration.h"
 
+#include "fdb5/config/Config.h"
 #include "fdb5/daos/DaosPool.h"
 
 namespace fdb5 {
-
 
 class DaosManager : private eckit::NonCopyable {
 
 public: // methods
 
-    // TODO: set configuration where relvant in DaosStore, DaosFieldLocation and in unit tests
-    // TODO: unit tests for config
     static DaosManager& instance() {
+
         static DaosManager instance;
         return instance;
+
     };
 
-    void configure(const eckit::LocalConfiguration& config) {
-        containerOidsPerAlloc_ = config.getLong("defaultCellSize", containerOidsPerAlloc_);
-        defaultCellSize_ = config.getLong("defaultCellSize", defaultCellSize_);
-        chunkCreationSize_ = config.getLong("defaultCellSize", chunkCreationSize_);
-    };
-
-private: // member
-
-    DaosManager() :
-        containerOidsPerAlloc_(100),
-        defaultCellSize_(1),
-        chunkCreationSize_(1048576) {
-
-        const Config& config = LibFdb5::defaultConfig();
-
-        if (config.has("daos")) {
-            auto daosConfig = config.getSubConfiguration("daos");
-            configure(daosConfig);
-        }
-    }
-
-public: //members
-
-    // TODO: move these to proper config
-    static const daos_size_t default_pool_create_scm_size = 10ULL << 30;
-    static const daos_size_t default_pool_create_nvme_size = 40ULL << 30;
-    static const int default_pool_destroy_force = 1;
+    fdb5::Config& config() { return config_; };
 
 private: // members
 
-    long containerOidsPerAlloc_;
-    long defaultCellSize_;
-    long chunkCreationSize_;
+    fdb5::Config config_{};
+
 };
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -80,7 +52,7 @@ class DaosSession : eckit::NonCopyable {
 
 public: // methods
 
-    DaosSession(const eckit::LocalConfiguration& config = eckit::LocalConfiguration());
+    DaosSession(const fdb5::Config& config = fdb5::Config());
     ~DaosSession();
 
     // administrative
@@ -98,16 +70,25 @@ public: // methods
 
 private: // methods
 
-    /// @todo: Use std::map<std::string, fdb5::DaosPool>
-    /// @todo: Offload caching to manager?
-    using PoolCache = std::deque<fdb5::DaosPool>;
-    PoolCache::iterator getCachedPool(uuid_t);
-    PoolCache::iterator getCachedPool(const std::string&);
+    std::deque<fdb5::DaosPool>::iterator getCachedPool(uuid_t);
+    std::deque<fdb5::DaosPool>::iterator getCachedPool(const std::string&);
+
+public: //members
+
+    static const daos_size_t default_pool_create_scm_size = 10ULL << 30;
+    static const daos_size_t default_pool_create_nvme_size = 40ULL << 30;
+    static const int default_pool_destroy_force = 1;
+
+    static const int default_container_oids_per_alloc = 100;
+
+    static const daos_size_t default_object_create_cell_size = 1;
+    static const daos_size_t default_object_create_chunk_size = 1048576;
 
 private: // members
 
-    PoolCache pool_cache_;
-    eckit::LocalConfiguration& config_;
+    std::deque<fdb5::DaosPool> pool_cache_;
+    fdb5::Config& config_;    
+
 };
 
 static inline int daos_call(int code, const char* msg, const char* file, int line, const char* func) {
