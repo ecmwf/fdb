a75a6e836d1d18305014ce2790c244bf52edaff1
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2023-09-16 09:09:25 +0100

    FDB-319 improved error message

--- src/fdb5/rules/Rule.cc
+++ src/fdb5/rules/Rule.cc
@@ -500,7 +500,12 @@ void Rule::check(const Key& key) const {
         if (k != key.end()) {
             const std::string& value = (*k).second;
             const Type& type = registry_.lookupType(pred->keyword());
-            ASSERT(value == type.tidy(pred->keyword(), value));
+            if (value != type.tidy(pred->keyword(), value)) {
+                std::stringstream ss;
+                ss << "Rule check - metadata not valid (not in canonical form) - found: ";
+                ss << pred->keyword() << "=" << value << " - expecting " << type.tidy(pred->keyword(), value) << std::endl;
+                throw eckit::UserError(ss.str(), Here());
+            }
         }
     }
     if (parent_ != nullptr) {
