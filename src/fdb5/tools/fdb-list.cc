277deac7a183964ff6698a3520236fe400a07ccd
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2023-11-17 14:47:42 +0000

    FDB-320 checking leaf structure (to handle optional metadata)

--- src/fdb5/tools/fdb-list.cc
+++ src/fdb5/tools/fdb-list.cc
@@ -64,6 +64,18 @@ class FDBList : public FDBVisitTool {
     bool compact_;
 };
 
+
+std::string keySignature(const fdb5::Key& key) {
+    std::string signature;
+    std::string separator="";
+    for (auto k : key.keys()) {
+        signature += separator+k;
+        separator=":";
+    }
+    return signature;
+}
+
+
 void FDBList::init(const CmdArgs& args) {
 
     FDBVisitTool::init(args);
@@ -116,7 +128,7 @@ void FDBList::execute(const CmdArgs& args) {
 
         // If --full is supplied, then include all entries including duplicates.
         auto listObject = fdb.list(request, !full_ && !compact_);
-        std::map<std::string, std::pair<metkit::mars::MarsRequest, std::unordered_set<Key>>> requests;
+        std::map<std::string, std::map<std::string, std::pair<metkit::mars::MarsRequest, std::unordered_set<Key>>>> requests;
 
         ListElement elem;
         while (listObject.next(elem)) {
@@ -129,12 +141,21 @@ void FDBList::execute(const CmdArgs& args) {
                 treeAxes += ",";
                 treeAxes += keys[1];
 
+                std::string signature=keySignature(keys[2]);
+
                 auto it = requests.find(treeAxes);
                 if (it == requests.end()) {
-                    requests.emplace(treeAxes, std::make_pair(keys[2].request(), std::unordered_set<Key>{keys[2]}));
+                    std::map<std::string, std::pair<metkit::mars::MarsRequest, std::unordered_set<Key>>> leaves;
+                    leaves.emplace(signature, std::make_pair(keys[2].request(), std::unordered_set<Key>{keys[2]}));
+                    requests.emplace(treeAxes, leaves);
                 } else {
-                    it->second.first.merge(keys[2].request());
-                    it->second.second.emplace(keys[2]);
+                    auto h = it->second.find(signature);
+                    if (h != it->second.end()) { // the hypercube request is already there... adding the 3rd level key
+                        h->second.first.merge(keys[2].request());
+                        h->second.second.insert(keys[2]);
+                    } else {
+                        it->second.emplace(signature, std::make_pair(keys[2].request(), std::unordered_set<Key>{keys[2]}));
+                    }
                 }
             } else {
                 if (json_) {
@@ -147,24 +168,25 @@ void FDBList::execute(const CmdArgs& args) {
         }
         if (compact_) {
             for (const auto& tree: requests) {
-                metkit::hypercube::HyperCube h{tree.second.first};
-                if (h.size() == tree.second.second.size()) {
-                    Log::info() << "retrieve," << tree.first << ",";
-                    tree.second.first.dump(Log::info(), "", "", false);
-                    Log::info() << std::endl;
-                } else {
-                    for (const auto& k: tree.second.second) {
-                        h.clear(k.request());
-                    }
-                    for (const auto& r: h.requests()) {
+                for (const auto& leaf: tree.second) {
+                    metkit::hypercube::HyperCube h{leaf.second.first};
+                    if (h.size() == leaf.second.second.size()) {
                         Log::info() << "retrieve," << tree.first << ",";
-                        r.dump(Log::info(), "", "", false);
+                        leaf.second.first.dump(Log::info(), "", "", false);
                         Log::info() << std::endl;
+                    } else {
+                        for (const auto& k: leaf.second.second) {
+                            h.clear(k.request());
+                        }
+                        for (const auto& r: h.requests()) {
+                            Log::info() << "retrieve," << tree.first << ",";
+                            r.dump(Log::info(), "", "", false);
+                            Log::info() << std::endl;
+                        }
                     }
                 }
             }
         }
-
         // n.b. finding no data is not an error for fdb-list
     }
 
