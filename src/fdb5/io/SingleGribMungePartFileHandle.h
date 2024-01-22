beb389f3819ac5f0fd29efc1e640db633e93b05f
Nicolau Manubens <nicolau.manubens@ecmwf.int>
2024-01-22 20:10:37 +0000

    Fix for fdb5/develop to be compatible with latest eckit/develop.

--- src/fdb5/io/SingleGribMungePartFileHandle.h
+++ src/fdb5/io/SingleGribMungePartFileHandle.h
@@ -66,7 +66,6 @@ public:
     bool canSeek() const override { return false; }
 
     void toRemote(eckit::Stream&) const override { NOTIMP; }
-    void cost(std::map<std::string,eckit::Length>&, bool) const override { NOTIMP; }
     std::string title() const override;
     bool moveable() const override { return true; }
     eckit::DataHandle* clone() const override;
