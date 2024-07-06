1a44c828befa81355d092f68dc286c954efa7c21
Metin Cakircali <metin.cakircali@ecmwf.int>
2024-07-06 15:42:27 +0200

    feat(FAM): added tests

---
+++ tests/fdb/fam/test_fam_common.h
@@ -0,0 +1,97 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*
+ * This software was developed as part of the Horizon Europe programme funded project OpenCUBE
+ * (Grant agreement: 101092984) horizon-opencube.eu
+ */
+
+/// @file   test_fam_common.h
+/// @author Metin Cakircali
+/// @date   Jun 2024
+
+#pragma once
+
+#include "eckit/config/YAMLConfiguration.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/filesystem/LocalPathName.h"
+#include "eckit/filesystem/PathName.h"
+#include "eckit/io/DataHandle.h"
+#include "eckit/log/Log.h"
+#include "fdb5/LibFdb5.h"
+
+#include <string.h>
+
+#include <fstream>
+#include <regex>
+
+using namespace std::string_literals;
+
+namespace {
+// const auto TEST_FDB_ENDPOINT = eckit::net::Endpoint("127.0.0.1", 8880);
+const auto TEST_FDB_FAM_ENDPOINT = "127.0.0.1:8880"s;
+const auto TEST_FDB_FAM_REGION   = "test_region_fdb"s;
+const auto TEST_FDB_FAM_URI      = "fam://" + TEST_FDB_FAM_ENDPOINT + "/" + TEST_FDB_FAM_REGION;
+}  // namespace
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#define TEST_LOG_INFO(msg)  eckit::Log::info() << "INFO  [TEST_FAM] : " << msg << std::endl
+#define TEST_LOG_DEBUG(msg) eckit::Log::debug<fdb5::LibFdb5>() << "DEBUG [TEST_FAM] : " << msg << std::endl
+
+//----------------------------------------------------------------------------------------------------------------------
+
+namespace fdb::test::fam {
+
+void readAndValidate(eckit::DataHandle* dh, const char* data, const long length) {
+    TEST_LOG_INFO("READ");
+
+    dh->openForRead();
+
+    std::string tmp;
+    tmp.resize(length);
+    char* buffer = &tmp[0];
+
+    const auto rlen = dh->read(buffer, length);
+
+    TEST_LOG_INFO("VALIDATE");
+
+    ASSERT(rlen == length);
+
+    ASSERT(::memcmp(data, buffer, length) == 0);
+
+    dh->close();
+}
+
+void write(const std::string& buffer, const eckit::PathName& path) {
+    std::ofstream file(path);
+    if (!file) { throw eckit::CantOpenFile(path); }
+    file << buffer;
+}
+
+struct FamSetup {
+    FamSetup(const std::string& schema, const std::string& config) {
+        // TEST_LOG_INFO("- Working directory: " << cwd_);
+        // TEST_LOG_INFO("- Config path: " << configPath);
+        // TEST_LOG_INFO("- Schema path: " << schemaPath);
+        cwd_.mkdir();
+        write(schema, schemaPath);
+        write(std::regex_replace(config, std::regex("./schema"), schemaPath.asString()), configPath.asString());
+    }
+
+    const eckit::LocalPathName cwd_ {eckit::LocalPathName::cwd() + "/" + "fam_test_dir"};
+
+    const eckit::PathName schemaPath {cwd_ + "/" + "schema"};
+    const eckit::PathName configPath {cwd_ + "/" + "config.yaml"};
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace fdb::test::fam
---
+++ tests/fdb/fam/test_fam_store.cc
@@ -0,0 +1,124 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*
+ * This software was developed as part of the Horizon Europe programme funded project OpenCUBE
+ * (Grant agreement: 101092984) horizon-opencube.eu
+ */
+
+/// @file   test_fam_store.cc
+/// @author Metin Cakircali
+/// @date   Jun 2024
+
+#include "eckit/testing/Test.h"
+#include "fdb5/config/Config.h"
+#include "fdb5/database/Key.h"
+#include "fdb5/fam/FamStore.h"
+#include "fdb5/rules/Schema.h"
+#include "test_fam_common.h"
+
+using namespace eckit::testing;
+
+namespace fdb::test {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+namespace {
+
+const std::string testSchema =
+    "[ fam1a, fam1b, fam1c\n" "    [ fam2a, fam2b, fam2c\n" "       [ fam3a, fam3b, fam3c ]]]\n";
+
+const std::string testConfig =
+    "---\n" "type: local\n" "schema: ./schema\n" "store: fam\n" "catalogue: toc\n" "spaces:\n" "- handler: Default\n" "  roots:\n" "  - path: ./root\n" "  - path: fam://endpoint/region\n" "  - path: ./backup\n" "    wipe: true\n" "    list: true\n" "    retrieve: false\n" "fam_roots:\n" "- uri: " +
+    TEST_FDB_FAM_URI + "\n" "  writable: true\n" "  visit: true\n";
+
+// std::unique_ptr<eckit::FamRegion> testRegion;
+
+}  // namespace
+
+//----------------------------------------------------------------------------------------------------------------------
+
+CASE("FamStore: create test region") {
+    auto name = eckit::FamRegionName(TEST_FDB_FAM_ENDPOINT, TEST_FDB_FAM_REGION);
+    try {
+        name.create(1024 * 4, 0640);
+    } catch (const eckit::AlreadyExists& e) { eckit::Log::debug() << "FamCommon: " << name << " already exists.\n"; }
+}
+
+CASE("FamStore: Archive, Retrieve, Remove") {
+    TEST_LOG_DEBUG("SETUP FAM STORE");
+
+    const fam::FamSetup setup(testSchema, testConfig);
+
+    const auto config = fdb5::Config {eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto& schema = config.schema();
+
+    const auto storeKey = fdb5::Key({{"fam1a", "val1a"}, {"fam1b", "val1b"}, {"fam1c", "val1c"}}, schema.registry());
+
+    fdb5::FamStore famStore(schema, storeKey, config);
+
+    auto& store = static_cast<fdb5::Store&>(famStore);
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    const char* value  = "Testing fam: ARCHIVE DATA!";
+    const auto  length = std::char_traits<char>::length(value);
+
+    const auto key = fdb5::Key({{"fam1a", "val1a"},
+                                {"fam1b", "val1b"},
+                                {"fam1c", "val1c"},
+                                {"fam2a", "val2a"},
+                                {"fam2b", "val2b"},
+                                {"fam2c", "val2c"},
+                                {"fam3a", "val3a"},
+                                {"fam3b", "val3b"},
+                                {"fam3c", "val3c"}},
+                               schema.registry());
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    TEST_LOG_INFO("ARCHIVE");
+
+    auto floc = store.archive(key, value, length);
+
+    EXPECT(floc && floc->length() == eckit::Length(length));
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    TEST_LOG_INFO("RETRIEVE");
+
+    auto field  = fdb5::Field(std::move(floc), std::time(nullptr));
+    auto handle = std::unique_ptr<eckit::DataHandle>(store.retrieve(field));
+
+    EXPECT(handle);
+
+    // handle->seek(-length);  // seek back (because of field loc)
+
+    fam::readAndValidate(handle.get(), value, length);
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //     TEST_LOG_INFO("REMOVE");
+    //
+    //     store.remove(key);
+    //
+    //     EXPECT_THROWS(fam::readAndValidate(handle.get(), value, length));
+
+    TEST_LOG_INFO("FINISHED!");
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace fdb::test
+
+int main(int argc, char** argv) {
+    return run_tests(argc, argv);
+}
