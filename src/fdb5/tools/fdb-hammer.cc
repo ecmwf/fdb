360fa5e3e97e71f57fe15ad3aaa74f940fbe372d
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2024-09-04 14:57:29 +0100

    multi-threded fdb-hammer

--- src/fdb5/tools/fdb-hammer.cc
+++ src/fdb5/tools/fdb-hammer.cc
@@ -21,10 +21,12 @@
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/SimpleOption.h"
 #include "eckit/option/VectorOption.h"
+#include "eckit/thread/ThreadPool.h"
 
 #include "fdb5/message/MessageArchiver.h"
 #include "fdb5/io/HandleGatherer.h"
 #include "fdb5/tools/FDBTool.h"
+#include "fdb5/api/helpers/FDBToolRequest.h"
 
 // This list is currently sufficient to get to nparams=200 of levtype=ml,type=fc
 const std::unordered_set<size_t> AWKWARD_PARAMS {11, 12, 13, 14, 15, 16, 49, 51, 52, 61, 121, 122, 146, 147, 169, 175, 176, 177, 179, 189, 201, 202};
@@ -96,6 +98,68 @@ void FDBWrite::execute(const eckit::option::CmdArgs &args) {
     }
 }
 
+
+// class for writing a chunk of the user buffer - used to perform multiple simultaneous writes
+class Init : public eckit::ThreadPoolTask {
+
+public:
+    Init(fdb5::FDB** fdb) : fdb_(fdb) {}
+
+    void execute() {
+        (*fdb_) = new fdb5::FDB();
+        auto it = (*fdb_)->list(fdb5::FDBToolRequest({}, true), true);
+        fdb5::ListElement el;
+        while(it.next(el)) {
+            // auto k = el.key();
+            // ASSERT(k.size() == 3);
+            // std::cout << k[0] << k[1] << k[2] << std::endl;
+        }
+    }
+
+private:
+    fdb5::FDB** fdb_;
+};
+
+// class for writing a chunk of the user buffer - used to perform multiple simultaneous writes
+class Archive : public eckit::ThreadPoolTask {
+
+public:
+    Archive(fdb5::FDB* fdb, const char* buffer, size_t size) : fdb_(fdb), size_(size) {
+        buffer_ = new char[size];
+        ::memcpy(buffer_, buffer, size);
+    }
+
+    void execute() {
+        fdb_->archive(buffer_, size_);
+        fdb_->flush();
+
+        delete[] buffer_;
+    }
+
+private:
+    fdb5::FDB* fdb_;
+    char* buffer_;
+    size_t size_;
+};
+
+class Retrieve : public eckit::ThreadPoolTask {
+
+public:
+    Retrieve(const eckit::option::CmdArgs& args, fdb5::HandleGatherer& handles, metkit::mars::MarsRequest request)
+        : args_(args), handles_(handles), request_(request) {
+    }
+
+    void execute() {
+        fdb5::FDB fdb(args_);
+        handles_.add(fdb.retrieve(request_));
+    }
+
+private:
+    const eckit::option::CmdArgs& args_;
+    fdb5::HandleGatherer& handles_;
+    metkit::mars::MarsRequest request_;
+};
+
 void FDBWrite::executeWrite(const eckit::option::CmdArgs &args) {
 
     eckit::AutoStdFile fin(args(0));
@@ -114,7 +178,7 @@ void FDBWrite::executeWrite(const eckit::option::CmdArgs &args) {
     const char* buffer = nullptr;
     size_t size = 0;
 
-    fdb5::MessageArchiver archiver(fdb5::Key(), false, verbose_, args);
+    // fdb5::MessageArchiver archiver(fdb5::Key(), false, verbose_, args);
 
     std::string expver = args.getString("expver");
     size = expver.length();
@@ -129,7 +193,21 @@ void FDBWrite::executeWrite(const eckit::option::CmdArgs &args) {
     size_t writeCount = 0;
     size_t bytesWritten = 0;
 
+    static size_t numThreads = 64;
     timer.start();
+    eckit::ThreadPool init("init", numThreads);
+    fdb5::FDB *fdb_[numThreads];
+    for (size_t f = 0; f< numThreads; f++) {
+        init.push(new Init(&(fdb_[f])));
+    }
+    init.wait();
+
+    std::cout << "###################### INIT completed #######################" << std::endl;
+
+    
+    eckit::ThreadPool pool("hammer", numThreads);
+
+    size_t i = 0;
 
     for (size_t member = 0; member < nensembles; ++member) {
         if (args.has("nensembles")) {
@@ -157,8 +235,11 @@ void FDBWrite::executeWrite(const eckit::option::CmdArgs &args) {
                     gribTimer.stop();
                     elapsed_grib += gribTimer.elapsed();
 
-                    MemoryHandle dh(buffer, size);
-                    archiver.archive(dh);
+                    pool.push(new Archive(fdb_[i], buffer, size));
+                    i = (i+1) % 4;
+
+
+                    // archiver.archive(dh);
                     writeCount++;
                     bytesWritten += size;
 
@@ -168,13 +249,15 @@ void FDBWrite::executeWrite(const eckit::option::CmdArgs &args) {
 
             gribTimer.stop();
             elapsed_grib += gribTimer.elapsed();
-            archiver.flush();
+            // archiver.flush();
             gribTimer.start();
         }
     }
 
     gribTimer.stop();
     elapsed_grib += gribTimer.elapsed();
+    
+    pool.wait();
 
     timer.stop();
 
@@ -211,7 +294,9 @@ void FDBWrite::executeRead(const eckit::option::CmdArgs &args) {
     timer.start();
 
     fdb5::HandleGatherer handles(false);
-    fdb5::FDB fdb(args);
+    eckit::ThreadPool pool("hammerRead", 80);
+
+    // fdb5::FDB fdb(args);
     size_t fieldsRead = 0;
 
     for (size_t member = 1; member <= nensembles; ++member) {
@@ -234,12 +319,14 @@ void FDBWrite::executeRead(const eckit::option::CmdArgs &args) {
                                 << ", level: " << level
                                 << ", param: " << real_param << std::endl;
 
-                    handles.add(fdb.retrieve(request));
+                    pool.push(new Retrieve(args, handles, request));
+                    // handles.add(fdb.retrieve(request));
                     fieldsRead++;
                 }
             }
         }
     }
+    pool.wait();
 
     std::unique_ptr<eckit::DataHandle> dh(handles.dataHandle());
 
