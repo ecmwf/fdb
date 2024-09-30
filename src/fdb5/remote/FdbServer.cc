27ad20524536261600c38a25a0777ae9c9c994ff
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2024-09-30 16:57:27 +0000

    wip

--- src/fdb5/remote/FdbServer.cc
+++ src/fdb5/remote/FdbServer.cc
@@ -118,7 +118,7 @@ void FdbServerBase::doRun() {
     startPortReaperThread(config);
 
     int port = config.getInt("serverPort", 7654);
-    bool threaded = config.getBool("serverThreaded", true);
+    bool threaded = config.getBool("serverThreaded", false);
 
     net::TCPServer server(net::Port("fdb", port), net::SocketOptions::server().reusePort(true));
     server.closeExec(false);
