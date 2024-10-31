0d61134b7b577f7ad5bb03fb009397ff8dcae34b
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2024-10-31 11:00:37 +0100

    sleep before connection

--- src/fdb5/remote/client/ClientConnectionRouter.cc
+++ src/fdb5/remote/client/ClientConnectionRouter.cc
@@ -1,5 +1,9 @@
 #include "fdb5/remote/client/ClientConnectionRouter.h"
 
+#include <chrono>
+#include <random>
+#include <thread>
+
 namespace{
     
 class ConnectionError : public eckit::Exception {
@@ -37,6 +41,17 @@ ClientConnection& ClientConnectionRouter::connection(const eckit::net::Endpoint&
         return *(it->second);
     } else {
         ClientConnection* clientConnection = new ClientConnection{endpoint, defaultEndpoint};
+        
+        static int fdbConnectMaxSleep = eckit::Resource<int>("fdbConnectMaxSleep", 0);
+        
+        if (fdbConnectMaxSleep) {
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_int_distribution<int> dist(0, fdbConnectMaxSleep);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(dist(rd)));
+        }
+
         if (clientConnection->connect()) {
             auto it = (connections_.emplace(endpoint, std::unique_ptr<ClientConnection>(clientConnection))).first;
             return *(it->second);
