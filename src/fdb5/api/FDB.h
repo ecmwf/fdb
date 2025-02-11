dd3f469e2b6cf872531200c9dff855947a73c571
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-02-11 10:58:03 +0100

    wip

--- src/fdb5/api/FDB.h
+++ src/fdb5/api/FDB.h
@@ -75,55 +75,49 @@ public:  // methods
     // -------------- Primary API functions ----------------------------
 
     /// Archive binary data to a FDB.
+    /// Any callback set with registerArchiveCallback will be invoked.
+    ///
     /// @param handle eckit::message::Message to data to archive
     void archive(eckit::message::Message msg);
 
     /// Archive binary data to a FDB.
     /// Reads messages from the eckit::DataHandle and calls archive() the the
     /// corresponding messages.
+    /// Any callback set with registerArchiveCallback will be invoked on each message.
+    ///
     /// @param handle eckit::DataHandle reference data to archive
     void archive(eckit::DataHandle& handle);
 
     /// Archive binary data to a FDB.
     /// Internally creates a DataHandle and calls archive().
+    /// Any callback set with registerArchiveCallback will be invoked on each message.
+    ///
     /// @param data Pointer to the binary data to archive
     /// @param length Size of the data to archive with the given
     void archive(const void* data, size_t length);
-    // warning: not high-perf API - makes sure that all the requested fields are archived and there are no data
-    // exceeding the request
-    
-    /** Archive binary data of a mars request and the corresponding binary data to a FDB.
-     *
-     * \warning not high-perf API - makes sure that all the requested fields are archived and there are no data exceeding the request
-     *
-     * \param request a mars request
-     * \param handle a data handle pointing to the data
-     */
 
-    /// Archive binary data of a mars request and the corresponding binary data to a FDB.
-    /// @warning not high-perf API - makes sure that all the requested fields are archived
-    /// and there are no data exceeding the request
+    /// Archives data from Datahandle and ensures all keys exactly match the provided MarsRequest.
+    /// Any callback set with registerArchiveCallback will be invoked on each message.
+    ///
     /// @param request a mars request
     /// @param handle a data handle pointing to the data
+    /// @throws eckit::UserError if there are more keys in the MarsRequest then in the messages.
+    /// @throws eckit::UserError if message key not present in MarsRequest.
     void archive(const metkit::mars::MarsRequest& request, eckit::DataHandle& handle);
 
     /// Archive binary data to a FDB.
-    /// @warning this is a low-level API. The provided key and the corresponding
-    /// data are not checked for consistency. Optional callback function is called upon
-    /// receiving field location from the store.
+    /// Any callback set with registerArchiveCallback will be invoked.
+    /// @note No constistency checks are applied. The caller needs to ensure the provided key matches metadata present
+    /// in data.
+    ///
     /// @param key Key used for indexing and archiving the data
     /// @param data Pointer to the binary data to archive
     /// @param length Size of the data to archive with the given @p key
     void archive(const Key& key, const void* data, size_t length);
 
+
     void reindex(const Key& key, const FieldLocation& location);
 
-    /// Flushes all buffers and closes all data handles into a consistent DB state
-    /// @note always safe to call
-    /** Flush all buffers and closes all data handles into a consistent DB state
-     *
-     * \note always safe to call
-     */
     /// Flush all buffers and closes all data handles into a consistent DB state
     /// @note always safe to call
     void flush();
@@ -152,37 +146,23 @@ public:  // methods
 
     ListIterator inspect(const metkit::mars::MarsRequest& request);
 
-    /** List data present at the archive and which can be retrieved
-     *
-     * \param request FDBToolRequest stating which data should be queried
-     * \param deduplicate bool whether the returned iterator should ignore duplicates
-     * \param length Size of the data to archive with the given @p key
-     *
-     * \return ListIterator for iterating over the set of found items
-     */
     /// List data present at the archive and which can be retrieved.
+    ///
     /// @param request FDBToolRequest stating which data should be queried
     /// @param deduplicate bool whether the returned iterator should ignore duplicates
     /// @param length Size of the data to archive with the given @p key
     /// @return ListIterator for iterating over the set of found items
     ListIterator list(const FDBToolRequest& request, bool deduplicate = false, int level = 3);
 
-    /** Dump the structural content of the FDB
-     *
-     * In particular, in the TOC formulation, enumerate the different entries 
-     * in the Table of Contents (including INIT and CLEAR entries).
-     * The dump will include information identifying the data files that are 
-     * referenced, and the "Axes" which describe the maximum possible extent of
-     * the data that is contained in the database.
-     * 
-     * \return DumpIterator for iterating over the set of found items
-     */
     /// Dump the structural content of the FDB
     /// In particular, in the TOC formulation, enumerate the different entries
     /// in the Table of Contents (including INIT and CLEAR entries).
     /// The dump will include information identifying the data files that are
     /// referenced, and the "Axes" which describe the maximum possible extent of
     /// the data that is contained in the database.
+    ///
+    /// @param request
+    /// @param simple
     /// @return DumpIterator for iterating over the set of found items
     DumpIterator dump(const FDBToolRequest& request, bool simple = false);
 
@@ -193,16 +173,19 @@ public:  // methods
     /// request to identify the database to delete. This is equivalent to a
     /// UNIX rm command.
     /// This tool deletes either whole databases, or whole indexes within databases
+    ///
     /// @param request FDBToolRequest stating which data should be queried
     /// @param doit flag for committing to the wipe (default is dry-run)
     /// @param porcelain flag print only a list of files to be deleted / that are deleted
     /// @param unsafeWipeAll flag for omitting all security checks and force a wipe
     /// @return WipeIterator for iterating over the set of wiped items
-    WipeIterator wipe(const FDBToolRequest& request, bool doit=false, bool porcelain=false, bool unsafeWipeAll=false);
+    WipeIterator wipe(const FDBToolRequest& request, bool doit = false, bool porcelain = false,
+                      bool unsafeWipeAll = false);
 
     /// Move content of one FDB database.
     /// This locks the source database, make it possible to create a second
     /// database in another root, duplicates all data. Source data are not automatically removed.
+    ///
     /// @param request a fdb tool request for the data which should be move
     /// @param dest destination uri to which the data should be moved
     /// @return MoveIterator for iterating over the set of found items
@@ -219,14 +202,16 @@ public:  // methods
     /// If an index refers to data that is not owned by the FDB
     /// (in particular data which has been adopted from an existing FDB4),
     /// this data will not be removed.
+    ///
     /// @param request a fdb tool request for the data which should be purged
     /// @param doit bool if true the purge is triggered, otherwise a dry-run is executed
     /// @param porcelain bool for printing only those files which are deleted
     /// @return PurgeIterator for iterating over the set of found items
-    PurgeIterator purge(const FDBToolRequest& request, bool doit=false, bool porcelain=false);
+    PurgeIterator purge(const FDBToolRequest& request, bool doit = false, bool porcelain = false);
 
     /// Prints information about FDB databases, aggregating the
     /// information over all the databases visited into a final summary.
+    ///
     /// @param request FDB tool request for which the stats should be shown
     /// @return StatsIterator for iterating over the set of found items
     StatsIterator stats(const FDBToolRequest& request);
@@ -236,21 +221,19 @@ public:  // methods
     /// @param action control action
     /// @param identifiers identifiers
     /// @return ControlIterator for iterating over the set of found items
-    ControlIterator control(const FDBToolRequest& request,
-                            ControlAction action,
-                            ControlIdentifiers identifiers);
+    ControlIterator control(const FDBToolRequest& request, ControlAction action, ControlIdentifiers identifiers);
 
     /// @todo Comment me pls
     /// @param request FDB tool request
     /// @param level maximum level the axis visitor should respect
     /// @return IndexAxis
-    IndexAxis axes(const FDBToolRequest& request, int level=3);
+    IndexAxis axes(const FDBToolRequest& request, int level = 3);
 
     /// @todo Comment me pls
     /// @param request FDB tool request
     /// @param level maximum level the axis visitor should respect
     /// @return AxisIterator
-    AxesIterator axesIterator(const FDBToolRequest& request, int level=3);
+    AxesIterator axesIterator(const FDBToolRequest& request, int level = 3);
 
     /// Check whether a specific control identifier is enabled
     /// @param controlIdentifier a given control identifier
@@ -277,6 +260,8 @@ public:  // methods
 
     FDBStats internalStats() const;
 
+    /// Name of fdb tyle, local or remote
+    ///
     const std::string& name() const;
 
     const Config& config() const;
