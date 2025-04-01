afd5103c5879fa5e97a0091b89deb8655294b6d7
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-04-01 08:59:28 +0200

    wip

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -11,5 +11,6 @@ if(HAVE_WARNINGS)
   ecbuild_add_cxx_flags("-Wno-unused-parameter" NO_FAIL)
   ecbuild_add_cxx_flags("-Wno-unused-variable" NO_FAIL)
   ecbuild_add_cxx_flags("-Wno-sign-compare" NO_FAIL)
+  ecbuild_add_cxx_flags("-Wglobal-constructors")
 
 endif()
