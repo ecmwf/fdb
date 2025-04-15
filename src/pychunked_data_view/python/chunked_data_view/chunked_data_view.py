654de65389308e453d5457230c59dc9eb2db4b1d
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-04-15 15:05:13 +0200

    Minor: Enable pytest from CMake
    Also introduced shim python layer.

---
+++ src/pychunked_data_view/python/chunked_data_view/chunked_data_view.py
@@ -0,0 +1,13 @@
+import pychunked_data_view
+
+def add(a: int, b: int):
+    """[TODO:description]
+
+    Args:
+        a: [TODO:description]
+        b: [TODO:description]
+
+    Returns:
+        [TODO:return]
+    """
+    return pychunked_data_view.add(a, b)
