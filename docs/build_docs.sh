4bdc9223cf450f604e119baf86d9cc750d568368
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-05-15 16:59:28 +0100

    Publish Fdb docs to sites.ecmwf

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
-doxygen_executable="${DOXYGEN_EXECUTABLE:-doxygen}"
-sphinx_executable="${SPHINX_EXECUTABLE:-sphinx-build}"
 script_dir="$(dirname "$(readlink -f "$0")")"
 doxyfile="${script_dir}/Doxyfile"
+doxygen_executable="${DOXYGEN_EXECUTABLE:-doxygen}"
+sphinx_executable="${SPHINX_EXECUTABLE:-sphinx-build}"
+version_str=$(cat "${script_dir}/../VERSION")
 output_dir=${DOCBUILD_OUTPUT:-doc-build}
 doxygen_output_dir="${output_dir}/doxygen"
 doxygen_input_dir="${script_dir}/../src/fdb5/api"
-version_str=$(cat "${script_dir}/../VERSION")
 
 mkdir -p "${doxygen_output_dir}"
+
+echo "PWD=$(pwd)"
 
 DOXYGEN_OUTPUT_DIR="${doxygen_output_dir}" \
     DOXYGEN_INPUT_DIR="${doxygen_input_dir}" \
     ${doxygen_executable} ${doxyfile}
 
-pushd "${output_dir}"
-"${sphinx_executable}" -b html -j auto \
-    -Dbreathe_projects.FDB=${doxygen_output_dir}/xml \
+"${sphinx_executable}" -j auto -E -a -T \
+    -Dbreathe_projects.FDB=$(readlink -f ${doxygen_output_dir}/xml) \
     -Dversion=${version_str} \
     -Drelease=${version_str} \
-    "${script_dir}" "sphinx"
-popd
+    "${script_dir}" "${output_dir}/sphinx"
