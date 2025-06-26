b30e109740a429eddfceb5a42935b6ba4b5b631f
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-06-26 15:27:14 +0200

    Feature: Testing for Axis + Restructuring

---
+++ src/chunked_data_view/RequestManipulation.cc
@@ -0,0 +1,45 @@
+#include "chunked_data_view/RequestManipulation.h"
+#include <cstddef>
+
+namespace chunked_data_view {
+
+void RequestManipulation::updateRequest(metkit::mars::MarsRequest& request, const Axis& axis, std::size_t chunkIndex) {
+    if (!axis.isChunked()) {
+        ASSERT(chunkIndex == 0);
+        for (const auto& [key, values] : axis.parameters()) {
+            request.values(key, values);
+        }
+        return;
+    }
+    ASSERT(chunkIndex < axis.size());
+
+    const auto dimCount = axis.parameters().size();
+    std::vector<size_t> index(dimCount, 0);
+
+    std::size_t chunk_index = chunkIndex;
+
+    // Idea: All dimension on the right side of the current index are multiplied and subtracted
+    // from the current index. The division in (1) computes the current index. (2) computes the
+    // remainder and afterwards we continue with the next index.
+    for (size_t i = 0; i < dimCount - 1; ++i) {
+        std::size_t dim_prod = 1;
+
+        for (size_t j = i+1; j < dimCount; ++j) {
+            dim_prod *= std::get<1>(axis.parameters()[j]).size();
+        }
+
+        std::size_t index_in_dim = chunk_index / dim_prod; // (1)
+        index[i] = index_in_dim;
+        chunk_index -= index_in_dim * dim_prod; // (2)
+    }
+
+    // Compute the final remainder
+    index[dimCount - 1] = chunk_index % std::get<1>(axis.parameters()[dimCount - 1]).size();
+
+    for (std::size_t i = 0; i < dimCount; ++i) {
+        const auto& [key, values] = axis.parameters()[i];
+        request.values(key, {values[index[i]]});
+    }
+}
+
+}  // namespace chunked_data_view
