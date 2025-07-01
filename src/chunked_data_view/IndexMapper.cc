8839abda9905c77c8d9dd0d2b3a3c03983248074
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-07-01 18:11:07 +0200

    Feature: Inspect FDB, order fields accordingly

---
+++ src/chunked_data_view/IndexMapper.cc
@@ -0,0 +1,140 @@
+#include "IndexMapper.h"
+#include "eckit/exception/Exceptions.h"
+
+namespace chunked_data_view {
+
+
+std::size_t IndexMapper::linearize(const std::vector<std::size_t>& indices, const std::vector<Axis>& axes) {
+
+    ASSERT(indices.size() == axes.size());
+
+    std::vector<std::size_t> axes_dimensions;
+    std::transform(axes.begin(), axes.end(), std::back_inserter(axes_dimensions),
+                   [](const chunked_data_view::Axis& axis) { return axis.isChunked() ? 1 : axis.size(); });
+
+    const auto dimCount = axes.size();
+    std::vector<std::size_t> dim_prods;
+
+    for (std::size_t i = 0; i < dimCount; ++i) {
+
+        std::size_t dim_prod = 1;
+
+        for (std::size_t j = i + 1; j < dimCount; ++j) {
+            dim_prod *= axes_dimensions[j];
+        }
+
+        dim_prods.push_back(dim_prod);
+    }
+
+    std::size_t result_index = 0;
+
+    for (std::size_t i = 0; i < dimCount; ++i) {
+        result_index += dim_prods[i] * indices[i];
+    }
+
+    return result_index;
+}
+
+std::vector<std::size_t> IndexMapper::delinearize(const std::size_t& index, const Axis& axis) {
+
+    if (index >= axis.size()) {
+        throw eckit::Exception("Index is out of bound for axis.");
+    }
+
+    if (axis.parameters().size() == 1) {
+        return {index};
+    }
+
+    const auto dimCount = axis.parameters().size();
+    std::vector<size_t> result(dimCount, 0);
+
+    std::size_t chunk_index = index;
+
+    // Idea: All dimension on the right side of the current index are multiplied and subtracted
+    // from the current index. The division in (1) computes the current index. (2) computes the
+    // remainder and afterwards we continue with the next index.
+    for (size_t i = 0; i < dimCount - 1; ++i) {
+        std::size_t dim_prod = 1;
+
+        for (size_t j = i + 1; j < dimCount; ++j) {
+            dim_prod *= axis.parameters()[j].values().size();
+        }
+
+        std::size_t index_in_dim = chunk_index / dim_prod;  // (1)
+        result[i]                = index_in_dim;
+        chunk_index -= index_in_dim * dim_prod;  // (2)
+    }
+
+    // Compute the final remainder
+    result[dimCount - 1] = chunk_index % axis.parameters()[dimCount - 1].values().size();
+
+    return result;
+}
+
+std::size_t IndexMapper::linearize(const std::vector<std::size_t>& indices, const Axis& axis) {
+
+    ASSERT(indices.size() == axis.parameters().size());
+
+    for (std::size_t i = 0; i < indices.size(); ++i) {
+        if (indices[i] >= axis.parameters()[i].values().size()) {
+            throw eckit::Exception("Out of bound exception: Index is out of bound for axis parameter");
+        }
+    }
+
+    if (indices.size() == 1) {
+        return indices[0];
+    }
+
+    const auto dimCount = axis.parameters().size();
+    std::vector<std::size_t> dim_prods;
+
+    for (std::size_t i = 0; i < dimCount; ++i) {
+
+        std::size_t dim_prod = 1;
+
+        for (std::size_t j = i + 1; j < dimCount; ++j) {
+            dim_prod *= axis.parameters()[j].values().size();
+        }
+
+        dim_prods.push_back(dim_prod);
+    }
+
+    std::size_t result = 0;
+
+    for (std::size_t i = 0; i < dim_prods.size(); ++i) {
+        result += indices[i] * dim_prods[i];
+    }
+
+    return result;
+}
+
+std::vector<std::size_t> IndexMapper::indexInAxisParameters(const Axis& axes, const fdb5::Key& key) {
+
+    // Sanity check whether the key is containing information about the axis
+    const std::vector<std::string> keys = key.names();
+
+    std::vector<std::size_t> result;
+
+    for (const auto& param : axes.parameters()) {
+
+        auto [it, success] = key.find(param.name());
+
+        if (!success) {
+            throw eckit::Exception("Couldn't find the parameter name in the keys of the request.");
+        }
+
+        // Find the index of the key value in the axis
+        auto res = std::find(param.values().begin(), param.values().end(), it->second);
+
+        if (res == param.values().end()) {
+            throw eckit::Exception("Couldn't request's key value in the axis.");
+        }
+
+        std::size_t index = std::distance(param.values().begin(), res);
+        result.push_back(index);
+    }
+
+    return result;
+}
+
+};  // namespace chunked_data_view
