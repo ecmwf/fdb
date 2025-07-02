1f007c8223c3dfa70c575dd066b83e34f6a9e7ee
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-07-02 14:39:50 +0200

    Refactoring

--- src/chunked_data_view/include/IndexMapper.h
+++ src/chunked_data_view/include/IndexMapper.h
@@ -5,16 +5,10 @@
 #include "chunked_data_view/Axis.h"
 #include "fdb5/database/Key.h"
 
-namespace chunked_data_view {
+namespace chunked_data_view::index_mapping {
 
-class IndexMapper {
-public:
-
-    static size_t linearize(const std::vector<std::size_t>& indices, const std::vector<Axis>& axes);
-    static size_t linearize(const std::vector<std::size_t>& indices, const Axis& axes);
-
-    static std::vector<size_t> delinearize(const std::size_t& index, const Axis& axis);
-
-    static std::vector<std::size_t> indexInAxisParameters(const Axis& axes, const fdb5::Key& key);
-};
-}  // namespace chunked_data_view
+size_t linearize(const std::vector<size_t>& indices, const std::vector<Axis>& axes);
+size_t linearize(const std::vector<size_t>& indices, const Axis& axes);
+std::vector<size_t> delinearize(const size_t& index, const Axis& axis);
+std::vector<size_t> indexInAxisParameters(const Axis& axes, const fdb5::Key& key);
+}  // namespace chunked_data_view::index_mapping
