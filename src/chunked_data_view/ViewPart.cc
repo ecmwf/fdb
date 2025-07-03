d1f1e04d5570e1a6839354e5ea2f8c68f08e0535
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-07-03 15:45:39 +0200

    Minor: Testing + Refactoring

--- src/chunked_data_view/ViewPart.cc
+++ src/chunked_data_view/ViewPart.cc
@@ -11,6 +11,7 @@
 #include <tuple>
 #include "chunked_data_view/Axis.h"
 #include "chunked_data_view/RequestManipulation.h"
+#include "chunked_data_view/ListIterator.h"
 
 namespace chunked_data_view {
 
@@ -61,8 +62,8 @@ void ViewPart::at(const std::vector<size_t>& chunkIndex, uint8_t* data, size_t s
     for (size_t idx = 0; idx < chunkIndex.size() - 1; ++idx) {
         RequestManipulation::updateRequest(request, axes_[idx], chunkIndex[idx]);
     }
-    auto dh = fdb_->inspect(request);
-    extractor_->writeInto(dh, axes_, layout_, data);
+    auto listIterator = fdb_->inspect(request);
+    extractor_->writeInto(std::move(listIterator), axes_, layout_, data);
 }
 
 metkit::mars::MarsRequest ViewPart::requestAt(const std::vector<size_t>& chunkIndex) const {
--- tests/chunked_data_view/test_index_mapper.cc
+++ tests/chunked_data_view/test_index_mapper.cc
@@ -3,54 +3,6 @@
 #include "eckit/testing/Test.h"
 #include "fdb5/api/helpers/FDBToolRequest.h"
 
-CASE("index_mapping | linearize | 1 axes 1 param | Chunked | Access valid") {
-
-    // Given
-    std::vector<std::string> dates              = {"20200101", "20200102", "20200103", "20200104"};
-    chunked_data_view::Parameter date_parameter = {"date", dates};
-    const chunked_data_view::Axis axis          = {{date_parameter}, true};
-
-    EXPECT(chunked_data_view::index_mapping::linearize({0}, axis) == 0);
-}
-
-
-CASE("index_mapping | linearize | 1 axes 1 param | Chunked | Access out of bounds") {
-
-    // Given
-
-    std::vector<std::string> dates              = {"20200101", "20200102", "20200103", "20200104"};
-    chunked_data_view::Parameter date_parameter = {"date", dates};
-    const chunked_data_view::Axis axis          = {{date_parameter}, true};
-
-    EXPECT_THROWS(chunked_data_view::index_mapping::linearize({4}, axis));
-}
-
-CASE("index_mapping | linearize | 1 axes 1 param | Chunked | Index too many dimensions") {
-
-    // Given
-    std::vector<std::string> dates              = {"20200101", "20200102", "20200103", "20200104"};
-    chunked_data_view::Parameter date_parameter = {"date", dates};
-    const chunked_data_view::Axis axis          = {{date_parameter}, true};
-
-    EXPECT_THROWS(chunked_data_view::index_mapping::linearize({0, 0}, axis));
-}
-
-CASE("index_mapping | linearize | 1 axes 2 param | Chunked | Valid access") {
-
-    // Given
-    std::vector<std::string> dates = {"20200101", "20200102", "20200103", "20200104"};
-    std::vector<std::string> times = {"0", "6", "12"};
-
-    chunked_data_view::Parameter date_parameter = {"date", dates};
-    chunked_data_view::Parameter time_parameter = {"time", times};
-
-    const chunked_data_view::Axis axis = {{date_parameter, time_parameter}, true};
-
-    EXPECT(chunked_data_view::index_mapping::linearize({0, 0}, axis) == 0);
-    EXPECT(chunked_data_view::index_mapping::linearize({0, 1}, axis) == 1);
-    EXPECT(chunked_data_view::index_mapping::linearize({0, 2}, axis) == 2);
-    EXPECT(chunked_data_view::index_mapping::linearize({3, 2}, axis) == 11);
-}
 
 CASE("index_mapping | delinearize | 1 axes 1 param | Chunked | Access valid") {
 
@@ -108,8 +60,8 @@ CASE("index_mapping | 2 axes 2/1 param | Chunked | Valid access") {
                                                        {{steps_parameter}, true}};
 
 
-    EXPECT(chunked_data_view::index_mapping::linearize({0, 0}, axes) == 0);
-    EXPECT(chunked_data_view::index_mapping::linearize({3, 0}, axes) == 0);
+    EXPECT(chunked_data_view::index_mapping::axis_index_to_buffer_index({0, 0}, axes) == 0);
+    EXPECT(chunked_data_view::index_mapping::axis_index_to_buffer_index({3, 0}, axes) == 0);
 }
 
 CASE("index_mapping | 3 axes 2/1 param | Chunked/Non-Chunked| Valid access") {
@@ -129,75 +81,10 @@ CASE("index_mapping | 3 axes 2/1 param | Chunked/Non-Chunked| Valid access") {
         {{date_parameter, time_parameter}, true}, {{steps_parameter}, false}, {{params_parameter}, true}};
 
 
-    EXPECT(chunked_data_view::index_mapping::linearize({0, 0, 0}, axes) == 0);
-    EXPECT(chunked_data_view::index_mapping::linearize({3, 0, 0}, axes) == 0);
+    EXPECT(chunked_data_view::index_mapping::axis_index_to_buffer_index({0, 0, 0}, axes) == 0);
+    EXPECT(chunked_data_view::index_mapping::axis_index_to_buffer_index({3, 0, 0}, axes) == 0);
 }
 
-// CASE("index_mapping | 4 axes 1 param | Chunked") {
-//
-//     // Given
-//     const std::string keys{
-//         "type=an,"
-//         "domain=g,"
-//         "expver=0001,"
-//         "stream=oper,"
-//         "date=2020-01-01/to/2020-01-04,"
-//         "levtype=sfc,"
-//         "param=v,"
-//         "step=0/1/2/3/4/5/6/7/8/9/10/11/12,"
-//         "time=0/6/12/18"};
-//
-//     auto request                    = fdb5::FDBToolRequest::requestsFromString(keys).at(0).request();
-//     std::vector<std::string> dates  = {"20200101", "20200102", "20200103", "20200104"};
-//     std::vector<std::string> times  = {"0000", "0600", "1200", "1800"};
-//     std::vector<std::string> steps  = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-//     std::vector<std::string> params = {"v"};
-//
-//     chunked_data_view::Parameter date_parameter  = {"date", dates};
-//     chunked_data_view::Parameter time_parameter  = {"time", times};
-//     chunked_data_view::Parameter step_parameter  = {"step", steps};
-//     chunked_data_view::Parameter param_parameter = {"param", params};
-//
-//     const std::vector<chunked_data_view::Axis> axes = {{{date_parameter}, true}, {{time_parameter}, true},
-//     {{step_parameter}, true}, {{param_parameter}, false}};
-//
-//     EXPECT(chunked_data_view::index_mapping::linearize({0, 0, 0, 0}, axes) == 0);
-//     EXPECT(chunked_data_view::index_mapping::linearize({0, 0, 1, 0}, axes) == 1);
-//     EXPECT(chunked_data_view::index_mapping::linearize({0, 1, 0, 0}, axes) == 13);
-//     EXPECT(chunked_data_view::index_mapping::linearize({1, 0, 0, 0}, axes) == 52);
-//
-// }
-
-// CASE("index_mapping | 1 axis 4 params | Non-chunked") {
-//
-//     // Given
-//     const std::string keys{
-//         "type=an,"
-//         "domain=g,"
-//         "expver=0001,"
-//         "stream=oper,"
-//         "date=2020-01-01/to/2020-01-04,"
-//         "levtype=sfc,"
-//         "param=v,"
-//         "step=0/1/2/3/4/5/6/7/8/9/10/11/12,"
-//         "time=0/6/12/18"};
-//
-//     auto request                    = fdb5::FDBToolRequest::requestsFromString(keys).at(0).request();
-//     std::vector<std::string> dates  = {"20200101", "20200102", "20200103", "20200104"};
-//     std::vector<std::string> times  = {"0000", "0600", "1200", "1800"};
-//     std::vector<std::string> steps  = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-//     std::vector<std::string> params = {"v"};
-//
-//     chunked_data_view::Parameter date_parameter  = {"date", dates};
-//     chunked_data_view::Parameter time_parameter  = {"time", times};
-//     chunked_data_view::Parameter step_parameter  = {"step", steps};
-//     chunked_data_view::Parameter param_parameter = {"param", params};
-//
-//     const chunked_data_view::Axis axis = {{date_parameter, time_parameter, step_parameter, param_parameter}, false};
-//
-//     chunked_data_view::index_mapping::linearize({0, 0, 0, 0}, {axis});
-//
-// }
 
 int main(int argc, char** argv) {
     return ::eckit::testing::run_tests(argc, argv);
