ccc9f06e468e866939abe0de47600f662ed0f335
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-07-04 16:27:41 +0200

    Bugfix: Wrong counting in part
    Skipping for chunk index fixed

---
+++ src/chunked_data_view/include/chunked_data_view/Buffer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+namespace chunked_data_view {
+
+class Buffer {
+    std::vector<double> data_;
+    std::vector<bool> bitset_;
+
+public:
+
+    Buffer() {};
+
+    Buffer(std::vector<double>& data) : data_(std::move(data)) { bitset_ = std::vector<bool>(data.size()); };
+
+    void resize(size_t size) {
+        data_.resize(size);
+        bitset_.resize(size);
+    }
+
+    size_t size() const { return data_.size(); }
+
+    size_t sizeOfValueType() const { return sizeof(decltype(data_)::value_type); }
+
+    auto dataPtr() { return data_.data(); }
+
+    std::vector<double>& values() { return data_; }
+};
+}  // namespace chunked_data_view
--- tests/z3fdb/test_store_v3.py
+++ tests/z3fdb/test_store_v3.py
@@ -64,7 +64,7 @@ def test_access(read_only_fdb_setup) -> None:
     print(data.shape)
 
 
-def test_axis_merge_check_out_of_bounds(read_only_fdb_setup_for_sfc_pl_example) -> None:
+def test_axis_check_out_of_bounds(read_only_fdb_setup_for_sfc_pl_example) -> None:
     """This test checks whether an access to an axis which has no pendant in the data is failing.
     The request below has param 167 which is not given in the data of the setup fdb. Therefore this
     needs to fail. Accessing the first two params is fine.
