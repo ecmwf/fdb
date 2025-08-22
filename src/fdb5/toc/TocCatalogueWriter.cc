9041cc62259fd5c6243144cd10fa656c12773297
Nicolau Manubens <nicolau.manubens@ecmwf.int>
2025-08-22 12:22:38 +0000

    Fix TocCatalogueWriter file-per-step.

--- src/fdb5/toc/TocCatalogueWriter.cc
+++ src/fdb5/toc/TocCatalogueWriter.cc
@@ -350,8 +350,8 @@ void TocCatalogueWriter::flush(size_t archivedFields) {
 
 eckit::PathName TocCatalogueWriter::generateIndexPath(const Key& key) const {
     eckit::PathName tocPath(directory_);
-    dpath /= std::string("flush" + eckit::Translator<int, std::string>()(flush_count_));
-    dpath.mkdir();
+    tocPath /= std::string("flush" + eckit::Translator<int, std::string>()(flush_count_));
+    tocPath.mkdir();
     tocPath /= key.valuesToString();
     tocPath = eckit::PathName::unique(tocPath) + ".index";
     return tocPath;
