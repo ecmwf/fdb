93467b8d521eb8251db72ee27207eec0f89163ce
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2025-09-15 15:47:30 +0100

    FDB-535 cleanup

--- src/fdb5/remote/client/RemoteCatalogue.h
+++ src/fdb5/remote/client/RemoteCatalogue.h
@@ -37,7 +37,6 @@ public:  // methods
 
     // From CatalogueWriter
     const Index& currentIndex() override;
-    bool selectIndex(const Key& idxKey) override;
     bool createIndex(const Key& idxKey, size_t datumKeySize) override;
     void archive(const Key& idxKey, const Key& datumKey, std::shared_ptr<const FieldLocation> fieldLocation) override;
     void overlayDB(const Catalogue& otherCatalogue, const std::set<std::string>& variableKeys, bool unmount) override;
@@ -49,6 +48,7 @@ public:  // methods
     bool retrieve(const Key& /*key*/, Field& /*field*/) const override { return false; }
 
     // From Catalogue
+    bool selectIndex(const Key& idxKey) override;
     const Key currentIndexKey() override;
     void deselectIndex() override;
     const Schema& schema() const override;
