f4e8f9cc80e7634c24df25dcfae5babc9425c973
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-11-04 11:40:47 +0100

    Minor: Wipe Implementation and tests

--- tests/pyfdb/conftest.py
+++ tests/pyfdb/conftest.py
@@ -12,6 +12,7 @@ import pytest
 import itertools
 import shutil
 import yaml
+import git
 
 import eccodes as ec
 
@@ -19,7 +20,7 @@ import pyfdb
 from pyfdb.pyfdb import Config
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def data_path() -> pathlib.Path:
     """
     Provides path to test data
@@ -29,12 +30,25 @@ def data_path() -> pathlib.Path:
     return path
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
+def test_data_path() -> pathlib.Path:
+    """
+    Provides path to test data
+    """
+
+    git_repo = git.Repo(__file__, search_parent_directories=True)
+    git_root = pathlib.Path(git_repo.git.rev_parse("--show-toplevel"))
+    path = git_root / "tests" / "pyfdb" / "data"
+    assert path.exists()
+    return path
+
+
+@pytest.fixture(scope="function")
 def session_tmp(tmp_path_factory) -> pathlib.Path:
     return tmp_path_factory.mktemp("session_data")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def build_grib_messages(data_path, session_tmp) -> pathlib.Path:
     template_grib = data_path / "template.grib"
     assert template_grib.is_file()
@@ -75,6 +89,76 @@ def build_grib_messages(data_path, session_tmp) -> pathlib.Path:
 
 @pytest.fixture(scope="session", autouse=False)
 def read_only_fdb_setup(data_path, session_tmp, build_grib_messages) -> pathlib.Path:
+    """
+    Creates a FDB setup in this tests temp directory.
+    Test FDB currently reads all grib files in `tests/data`
+    This setup can be shared between tests as we will only read
+    data from this FDB
+    """
+    schema_path_src = data_path / "schema"
+    assert schema_path_src.is_file()
+    schema_path = session_tmp / "schema"
+    shutil.copy(schema_path_src, schema_path)
+
+    db_store_path = session_tmp / "db_store"
+    db_store_path.mkdir()
+    fdb_config = dict(
+        type="local",
+        engine="toc",
+        schema=str(schema_path),
+        spaces=[
+            dict(
+                handler="Default",
+                roots=[
+                    {"path": str(db_store_path)},
+                ],
+            )
+        ],
+    )
+    fdb_config_str = yaml.dump(fdb_config)
+    fdb_config_path = session_tmp / "fdb_config.yaml"
+    fdb_config_path.write_text(fdb_config_str)
+    pyfdb_config = Config(fdb_config_str)
+    fdb = pyfdb.PyFDB(pyfdb_config)
+    fdb.archive(build_grib_messages.read_bytes())
+    fdb.flush()
+    return fdb_config_path
+
+
+@pytest.fixture(scope="function", autouse=False)
+def empty_fdb_setup(data_path, session_tmp) -> pathlib.Path:
+    """
+    Creates a FDB setup in this tests temp directory.
+    This setup can be shared between tests
+    """
+    schema_path_src = data_path / "schema"
+    assert schema_path_src.is_file()
+    schema_path = session_tmp / "schema"
+    shutil.copy(schema_path_src, schema_path)
+
+    db_store_path = session_tmp / "db_store"
+    db_store_path.mkdir()
+    fdb_config = dict(
+        type="local",
+        engine="toc",
+        schema=str(schema_path),
+        spaces=[
+            dict(
+                handler="Default",
+                roots=[
+                    {"path": str(db_store_path)},
+                ],
+            )
+        ],
+    )
+    fdb_config_str = yaml.dump(fdb_config)
+    fdb_config_path = session_tmp / "fdb_config.yaml"
+    fdb_config_path.write_text(fdb_config_str)
+    return fdb_config_path
+
+
+@pytest.fixture(scope="function", autouse=False)
+def read_write_fdb_setup(data_path, session_tmp, build_grib_messages) -> pathlib.Path:
     """
     Creates a FDB setup in this tests temp directory.
     Test FDB currently reads all grib files in `tests/data`
