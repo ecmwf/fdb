c42e8ef70b80e5e7e629685bfb1c15fedb930024
Tobias Kremer <tobias.kremer@ecmwf.int>
2025-11-10 11:02:44 +0100

    Minor: Add ECMWF Disclaimer to python files

--- src/pyfdb/pyfdb_iterator.py
+++ src/pyfdb/pyfdb_iterator.py
@@ -1,8 +1,14 @@
-from typing import Dict, List, Tuple
-from pyfdb import DataHandle
+# (C) Copyright 2025- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
 
-from pyfdb import URI
+from typing import Dict, List
 
+from pyfdb import URI, DataHandle
 from pyfdb_bindings import pyfdb_bindings as pyfdb_internal
 
 
