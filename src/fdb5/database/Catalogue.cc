f4b9e996971843271aff95c4f78379072b82224b
Chris Bradley <chris.bradley@ecmwf.int>
2025-11-18 22:53:18 +0000

    Remove unused headers

--- src/fdb5/database/Catalogue.cc
+++ src/fdb5/database/Catalogue.cc
@@ -8,11 +8,8 @@
  * does it submit to any jurisdiction.
  */
 
-#include <_stdio.h>
-#include <cstring>
 #include <map>
 
-#include "eckit/config/Resource.h"
 #include "eckit/exception/Exceptions.h"
 #include "eckit/io/AutoCloser.h"
 #include "eckit/thread/AutoLock.h"
