541cafbc1032a42063c09c32633cafca8219e9e5
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-12-19 15:51:59 +0000

    Fix FDB-610
    ReadVisitor now uses correct allias when accessing values. Code was not
    accounting for Month/Year type truncations on dates and other types.

--- src/fdb5/database/ReadVisitor.cc
+++ src/fdb5/database/ReadVisitor.cc
@@ -8,3 +8,25 @@
  * does it submit to any jurisdiction.
  */
 #include "fdb5/database/ReadVisitor.h"
+
+namespace fdb5 {
+
+void ReadVisitor::values(const metkit::mars::MarsRequest& request, const std::string& keyword,
+                         const TypesRegistry& registry, eckit::StringList& values) {
+    const auto& type = registry.lookupType(keyword);
+    eckit::StringList list;
+    type.getValues(request, keyword, list, wind_, catalogue_);
+
+    std::optional<std::reference_wrapper<const eckit::DenseSet<std::string>>> filter;
+    if (catalogue_) {
+        filter = catalogue_->axis(type.alias());
+    }
+
+    for (const auto& value : list) {
+        const std::string v = type.toKey(value);
+        if (!filter || filter->get().find(v) != filter->get().end()) {
+            values.push_back(value);
+        }
+    }
+}
+}  // namespace fdb5
