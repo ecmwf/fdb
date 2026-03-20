22e4c4fc891b975bf9bb936f2603f9a0507d027c
Tobias Kremer <tobias.kremer@ecmwf.int>
2026-03-20 17:24:01 +0100

    feat: Fixed the failing test cases:
    - xfail for the test case linked to FDB-652
    - adjusted the expected output for the other test cases

--- tests/fdb_e2e/tool_tests/no_subtocs/wipe/porcelain.sh
+++ tests/fdb_e2e/tool_tests/no_subtocs/wipe/porcelain.sh
@@ -17,17 +17,12 @@ fdb-list --all --full --minimum-keys="" --porcelain | tee out
 fdb-wipe class=rd,expver=xxxx,stream=oper,date=-1,time=1200 --porcelain | tee wipe_out
 fdb-list --all --full --minimum-keys="" --porcelain | tee out
 [[ "$(wc -l < out | tr -d '[:space:]')" != "24" ]] && echo "FDB modified without --doit" && exit -1
-
-# TODO(TKR): The file format has been altered. We either need to fix this here or adjust the output
-#            Directory is currenlty not included
-[[ "$(wc -l < wipe_out | tr -d '[:space:]')" != "5" ]] && echo "Unexpected file list for removal" && exit -1 # n.b. includes directory
+[[ "$(wc -l < wipe_out | tr -d '[:space:]')" != "11" ]] && echo "Unexpected file list for removal" && exit -1 # n.b. includes directory
 
 fdb-wipe class=rd,expver=xxxx,stream=oper,date=-1,time=1200 --doit --porcelain | tee wipe_out
 fdb-list --all --full --minimum-keys="" --porcelain | tee out
 [[ "$(wc -l < out | tr -d '[:space:]')" != "12" ]] && echo "Incorrect amount of data removed" && exit -1
-# TODO(TKR): The file format has been altered. We either need to fix this here or adjust the output
-#            Directory is currenlty not included
-[[ "$(wc -l < wipe_out | tr -d '[:space:]')" != "8" ]] && echo "Unexpected file list for removal" && exit -1
+[[ "$(wc -l < wipe_out | tr -d '[:space:]')" != "24" ]] && echo "Unexpected file list for removal" && exit -1
 [[ "$(grep time=1200 out | wc -l | tr -d '[:space:]')" != "0" ]] && echo "Incorrect amount of data removed" && exit -1
 [[ "$(grep time=0000 out | wc -l | tr -d '[:space:]')" != "12" ]] && echo "Incorrect amount of data removed" && exit -1
 [[ "$(find . -name '*.data' | wc -l | tr -d '[:space:]')" != "1" ]] && echo "Incorrect data files removed" && exit -1
