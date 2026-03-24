f2736054262821e7e0aa7fe33a7152a7aa885b02
Metin Cakircali <metin.cakircali@ecmwf.int>
2026-03-24 14:03:26 +0100

    test(fam): increase coverage store
    FDB-528

--- tests/fdb/fam/test_fam_store.cc
+++ tests/fdb/fam/test_fam_store.cc
@@ -22,6 +22,8 @@
 #include <cstring>
 #include <ctime>
 #include <memory>
+#include <set>
+#include <sstream>
 #include <string>
 #include <utility>
 
@@ -35,6 +37,8 @@
 #include "fdb5/database/Field.h"
 #include "fdb5/database/Key.h"
 #include "fdb5/database/Store.h"
+#include "fdb5/database/WipeState.h"
+#include "fdb5/fam/FamFieldLocation.h"
 #include "fdb5/fam/FamStore.h"
 #include "fdb5/rules/Schema.h"
 
@@ -245,6 +249,345 @@ CASE("FamStore: URI-based construction") {
     fam::read_and_validate(handle.get(), data, data_length);
 
     TEST_LOG_INFO("FINISHED!");
+}
+
+CASE("FamStore: asCollocatedDataURIs validates URIs") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    auto& store = static_cast<fdb5::Store&>(fam_store);
+
+    // Archive to get a valid URI
+    const char* data = "collocated-test-data";
+    const auto data_length = std::char_traits<char>::length(data);
+
+    auto key = fdb5::Key({{"fam1a", "v1a"},
+                          {"fam1b", "v1b"},
+                          {"fam1c", "v1c"},
+                          {"fam2a", "v2a"},
+                          {"fam2b", "v2b"},
+                          {"fam2c", "v2c"},
+                          {"fam3a", "v3a"},
+                          {"fam3b", "v3b"},
+                          {"fam3c", "v3c"}});
+
+    auto floc = store.archive(key, data, data_length);
+    EXPECT(floc);
+
+    // Valid URI should pass through
+    std::set<eckit::URI> valid_uris{floc->uri()};
+    auto result = store.asCollocatedDataURIs(valid_uris);
+    EXPECT_EQUAL(result.size(), 1U);
+
+    // Empty set should return empty set
+    std::set<eckit::URI> empty_uris;
+    EXPECT(store.asCollocatedDataURIs(empty_uris).empty());
+
+    // Foreign URI should throw
+    std::set<eckit::URI> foreign_uris{eckit::URI("fam://other:1234/foreign_region/obj")};
+    EXPECT_THROWS_AS(store.asCollocatedDataURIs(foreign_uris), eckit::BadValue);
+}
+
+CASE("FamStore: getAuxiliaryURIs returns empty") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    const auto fam_uri = eckit::URI(test_fdb_fam_uri + "/some_object");
+    auto aux = fam_store.getAuxiliaryURIs(fam_uri, true);
+    EXPECT(aux.empty());
+
+    auto aux2 = fam_store.getAuxiliaryURIs(fam_uri, false);
+    EXPECT(aux2.empty());
+}
+
+CASE("FamStore: print outputs store info") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    std::ostringstream out;
+    out << fam_store;
+    EXPECT(out.str().find("FamStore") != std::string::npos);
+}
+
+CASE("FamStore: remove deallocates archived object") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    auto& store = static_cast<fdb5::Store&>(fam_store);
+
+    const char* data = "data-to-remove";
+    const auto data_length = std::char_traits<char>::length(data);
+
+    auto key = fdb5::Key({{"fam1a", "v1a"},
+                          {"fam1b", "v1b"},
+                          {"fam1c", "v1c"},
+                          {"fam2a", "v2a"},
+                          {"fam2b", "v2b"},
+                          {"fam2c", "v2c"},
+                          {"fam3a", "v3a"},
+                          {"fam3b", "v3b"},
+                          {"fam3c", "v3c"}});
+
+    auto floc = store.archive(key, data, data_length);
+    EXPECT(floc);
+    EXPECT(store.uriExists(floc->uri()));
+
+    // remove with doit=true
+    std::ostringstream log_always, log_verbose;
+    store.remove(floc->uri(), log_always, log_verbose, true);
+
+    EXPECT(!store.uriExists(floc->uri()));
+
+    // Removing again should not throw (already gone)
+    EXPECT_NO_THROW(store.remove(floc->uri(), log_always, log_verbose, true));
+
+    // remove with doit=false (dry run): should not throw
+    auto floc2 = store.archive(key, data, data_length);
+    EXPECT(floc2);
+    store.remove(floc2->uri(), log_always, log_verbose, false);
+    // Object should still exist after dry run
+    EXPECT(store.uriExists(floc2->uri()));
+}
+
+CASE("FamStore: doUnsafeFullWipe returns false") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    EXPECT(!fam_store.doUnsafeFullWipe());
+}
+
+CASE("FamStore: doWipeEmptyDatabase is a no-op") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    EXPECT_NO_THROW(fam_store.doWipeEmptyDatabase());
+}
+
+CASE("FamStore: doWipeUnknowns removes matching objects") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    auto& store = static_cast<fdb5::Store&>(fam_store);
+
+    const char* data = "unknown-wipe-data";
+    const auto data_length = std::char_traits<char>::length(data);
+
+    auto key = fdb5::Key({{"fam1a", "v1a"},
+                          {"fam1b", "v1b"},
+                          {"fam1c", "v1c"},
+                          {"fam2a", "v2a"},
+                          {"fam2b", "v2b"},
+                          {"fam2c", "v2c"},
+                          {"fam3a", "v3a"},
+                          {"fam3b", "v3b"},
+                          {"fam3c", "v3c"}});
+
+    auto floc = store.archive(key, data, data_length);
+    EXPECT(floc);
+
+    std::set<eckit::URI> unknowns{floc->uri()};
+    EXPECT(fam_store.doWipeUnknowns(unknowns));
+    EXPECT(!store.uriExists(floc->uri()));
+
+    // Foreign URIs are skipped but don't cause failure
+    std::set<eckit::URI> foreign{eckit::URI("fam://other:1234/foreign_region/obj")};
+    EXPECT(fam_store.doWipeUnknowns(foreign));
+}
+
+CASE("FamStore: doWipeURIs removes included and auxiliary URIs") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    auto& store = static_cast<fdb5::Store&>(fam_store);
+
+    const char* data = "wipe-uris-data";
+    const auto data_length = std::char_traits<char>::length(data);
+
+    auto key = fdb5::Key({{"fam1a", "v1a"},
+                          {"fam1b", "v1b"},
+                          {"fam1c", "v1c"},
+                          {"fam2a", "v2a"},
+                          {"fam2b", "v2b"},
+                          {"fam2c", "v2c"},
+                          {"fam3a", "v3a"},
+                          {"fam3b", "v3b"},
+                          {"fam3c", "v3c"}});
+
+    auto floc = store.archive(key, data, data_length);
+    EXPECT(floc);
+
+    fdb5::StoreWipeState wipeState(fam_store.uri());
+    wipeState.includeData(floc->uri());
+
+    EXPECT(fam_store.doWipeURIs(wipeState));
+    EXPECT(!store.uriExists(floc->uri()));
+}
+
+CASE("FamStore: finaliseWipeState with empty URIs returns immediately") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    fdb5::StoreWipeState wipeState(fam_store.uri());
+    EXPECT_NO_THROW(fam_store.finaliseWipeState(wipeState, false, false));
+}
+
+CASE("FamStore: finaliseWipeState with existing URI") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    auto& store = static_cast<fdb5::Store&>(fam_store);
+
+    const char* data = "finalise-data";
+    const auto data_length = std::char_traits<char>::length(data);
+
+    auto key = fdb5::Key({{"fam1a", "v1a"},
+                          {"fam1b", "v1b"},
+                          {"fam1c", "v1c"},
+                          {"fam2a", "v2a"},
+                          {"fam2b", "v2b"},
+                          {"fam2c", "v2c"},
+                          {"fam3a", "v3a"},
+                          {"fam3b", "v3b"},
+                          {"fam3c", "v3c"}});
+
+    auto floc = store.archive(key, data, data_length);
+    EXPECT(floc);
+
+    fdb5::StoreWipeState wipeState(fam_store.uri());
+    wipeState.includeData(floc->uri());
+    fam_store.finaliseWipeState(wipeState, true, false);
+    EXPECT(wipeState.missingURIs().empty());
+}
+
+CASE("FamStore: checkUID is a no-op") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    EXPECT_NO_THROW(fam_store.checkUID());
+}
+
+CASE("FamStore: open returns true") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+    EXPECT(fam_store.open());
+}
+
+CASE("FamStore: static uri resolves correctly") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const eckit::URI fam_uri(test_fdb_fam_uri + "/some_object");
+    auto resolved = fdb5::FamStore::uri(fam_uri);
+    EXPECT_EQUAL(resolved.scheme(), std::string("fam"));
+}
+
+CASE("FamStore: finaliseWipeState throws on non-belonging URI") {
+
+    eckit::FamRegionName(fam::test_fdb_fam_endpoint, test_fdb_fam_region)
+        .create(test_region_size, test_region_perm, true);
+
+    const fam::FamSetup setup(test_schema, test_config);
+    const auto config = fdb5::Config{eckit::YAMLConfiguration(setup.configPath)};
+
+    const auto store_key = fdb5::Key({{"fam1a", "v1a"}, {"fam1b", "v1b"}, {"fam1c", "v1c"}});
+
+    fdb5::FamStore fam_store(store_key, config);
+
+    // Include a URI that doesn't belong to this store
+    fdb5::StoreWipeState wipeState(fam_store.uri());
+    wipeState.includeData(eckit::URI("fam://other:1234/foreign_region/obj"));
+    EXPECT_THROWS_AS(fam_store.finaliseWipeState(wipeState, true, false), eckit::SeriousBug);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
