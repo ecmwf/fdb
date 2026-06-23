9a99e1d3780a20a42232cd600c028215eb69b39a
Tobias Kremer <tobias.kremer@ecmwf.int>
2026-06-23 08:48:20 +0200

    fix: clang format

--- src/chunked_data_view/IndexMapper.h
+++ src/chunked_data_view/IndexMapper.h
@@ -17,8 +17,7 @@
 namespace chunked_data_view::index_mapping {
 
 size_t computeBufferIndex(const std::vector<Axis>& axes, const fdb5::Key& key,
-                          const std::vector<size_t>& partAxisOffset,
-                          const std::vector<size_t>& bufferOffset,
+                          const std::vector<size_t>& partAxisOffset, const std::vector<size_t>& bufferOffset,
                           const std::vector<size_t>& bufferExtent);
 std::vector<size_t> to_axis_parameter_index(const size_t& index, const Axis& axis);
 }  // namespace chunked_data_view::index_mapping
