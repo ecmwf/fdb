a29727a30b1dd704426c8245ec16c58df0f2cdc1
Vlad Pankratov <vlad@oxidian.com>
2026-06-24 01:22:29 +0200

    Remove unused exception throwing methods and related tests from ExceptionTest

--- rust/crates/fdb-sys/cpp/ExceptionTest.h
+++ rust/crates/fdb-sys/cpp/ExceptionTest.h
@@ -10,10 +10,7 @@ class ExceptionTest {
 public:
 
     static void throw_eckit_exception();
-    static void throw_eckit_serious_bug();
-    static void throw_eckit_user_error();
     static void throw_std_exception();
-    static void throw_int();
 };
 
 //----------------------------------------------------------------------------------------------------------------------
