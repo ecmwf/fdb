9b7efde13767c1624acdc31de600e12097930387
Tobias Kremer <tobias.kremer@ecmwf.int>
2026-07-02 14:13:35 +0200

    feat(fdb-719): Add cli env vars to pytest execution
    Pass cli arguments to the execution of pytest
    docs: Added README.md intructions
    feat: Split ctests into individual tool tests
    feat: Individual ctests for pytest marks
    fix: Correct the tmp folder for test execution
    chore: Remove incomplete api tests

--- tests/fdb_e2e/tool_tests/util/run_sh.py
+++ tests/fdb_e2e/tool_tests/util/run_sh.py
@@ -3,11 +3,6 @@ import subprocess
 from pathlib import Path
 from typing import Optional
 
-# def run_script(script: Path, cwd, env):
-#     result = subprocess.run([str(script)], cwd=cwd, env=env, capture_output=True, text=True)
-#     if result.returncode != 0:
-#         raise AssertionError(f"Shell script failed: {script.name}\nstdout:\n{result.stdout}\nstderr:\n{result.stderr}")
-
 
 def run_script(script: Path, args: Optional[list[str]], cwd: str, env: dict[str, str]):
     command = [str(script)]
@@ -20,7 +15,13 @@ def run_script(script: Path, args: Optional[list[str]], cwd: str, env: dict[str,
     logging.debug(env)
 
     process = subprocess.Popen(
-        command, cwd=cwd, env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, text=True
+        command,
+        cwd=cwd,
+        env=env,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        bufsize=1,
+        text=True,
     )
 
     stdout = ""
@@ -34,6 +35,8 @@ def run_script(script: Path, args: Optional[list[str]], cwd: str, env: dict[str,
     process.wait()
 
     if process.returncode != 0:
-        raise subprocess.CalledProcessError(returncode=process.returncode, cmd=str(script))
+        raise subprocess.CalledProcessError(
+            returncode=process.returncode, cmd=str(script)
+        )
 
     return stdout
