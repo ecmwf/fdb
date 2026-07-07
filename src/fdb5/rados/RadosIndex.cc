52eafdf881ceb32ef544376df0cba94d094059df
Metin Cakircali <metin.cakircali@ecmwf.int>
2026-07-07 08:52:43 +0200

    fix(rados): index

--- src/fdb5/rados/RadosIndex.cc
+++ src/fdb5/rados/RadosIndex.cc
@@ -8,33 +8,55 @@
  * does it submit to any jurisdiction.
  */
 
-#include <limits.h>  // for PATH_MAX
+#include "fdb5/rados/RadosIndex.h"
 
-#include <tuple>
-
+#include "eckit/exception/Exceptions.h"
+#include "eckit/filesystem/URI.h"
+#include "eckit/io/DataHandle.h"
+#include "eckit/io/Length.h"
 #include "eckit/io/MemoryHandle.h"
-#include "eckit/serialisation/MemoryStream.h"
+#include "eckit/io/rados/RadosException.h"
+#include "eckit/io/rados/RadosKeyValue.h"
+#include "eckit/io/rados/RadosNamespace.h"
 #include "eckit/serialisation/HandleStream.h"
+#include "eckit/serialisation/MemoryStream.h"
+#include "eckit/serialisation/Reanimator.h"
 #include "eckit/utils/Tokenizer.h"
 
-#include "fdb5/rados/RadosIndex.h"
+#include "fdb5/database/EntryVisitMechanism.h"
+#include "fdb5/database/Field.h"
+#include "fdb5/database/FieldDetails.h"
+#include "fdb5/database/FieldLocation.h"
+#include "fdb5/database/Index.h"
+#include "fdb5/database/Key.h"
 #include "fdb5/rados/RadosLazyFieldLocation.h"
+
+#include <climits>  // for PATH_MAX
+#include <cstddef>
+#include <ctime>
+#include <iterator>
+#include <memory>
+#include <set>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 // #if defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_WRITE) || defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH)
 // eckit::RadosPersistentKeyValue buildIndexKvName(const fdb5::Key& key, const eckit::RadosNamespace& name) {
 // #else
 // eckit::RadosKeyValue buildIndexKvName(const fdb5::Key& key, const eckit::RadosNamespace& name) {
 // #endif
-    /// create index kv
-    /// @todo: pass oclass from config
-    /// @todo: hash string into lower oid bits
+/// create index kv
+/// @todo: pass oclass from config
+/// @todo: hash string into lower oid bits
 
 // #ifdef fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_WRITE
 //     return eckit::RadosPersistentKeyValue{name.poolName(), name.containerName(), key.valuesToString(), true};
 // #elif fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH
 //     return eckit::RadosPersistentKeyValue{name.poolName(), name.containerName(), key.valuesToString()};
 // #else
-    // return eckit::RadosKeyValue{name.poolName(), name.containerName(), key.valuesToString()};
+// return eckit::RadosKeyValue{name.poolName(), name.containerName(), key.valuesToString()};
 // #endif
 
 // }
@@ -43,8 +65,8 @@ namespace fdb5 {
 
 //----------------------------------------------------------------------------------------------------------------------
 
-RadosIndex::RadosIndex(const Key& key, const eckit::RadosNamespace& name) : 
-    IndexBase(key, "radosKeyValue"), 
+RadosIndex::RadosIndex(const Key& key, const eckit::RadosNamespace& name) :
+    IndexBase(key, "radosKeyValue"),
     location_(eckit::RadosKeyValue{name.pool().name(), name.name(), key.valuesToString()}, 0),
     idx_kv_(location_.radosName().uri()) {
 
@@ -53,7 +75,7 @@ RadosIndex::RadosIndex(const Key& key, const eckit::RadosNamespace& name) :
     /// - create/open index kv (daos_kv_open)
 
     /// write indexKey under "key"
-    eckit::MemoryHandle h{(size_t) PATH_MAX};
+    eckit::MemoryHandle h{(size_t)PATH_MAX};
     eckit::HandleStream hs{h};
     h.openForWrite(eckit::Length(0));
     {
@@ -69,26 +91,23 @@ RadosIndex::RadosIndex(const Key& key, const eckit::RadosNamespace& name) :
     /// @note: performed RPCs:
     /// - record index key into index kv (daos_kv_put)
     idx_kv_.put("key", h.data(), hs.bytesWritten());
-    
 }
 
 // #if defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_WRITE) || defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH)
 // RadosIndex::RadosIndex(const Key& key, const eckit::RadosPersistentKeyValue& name, bool readAxes) :
 // #else
 RadosIndex::RadosIndex(const Key& key, const eckit::RadosKeyValue& name, bool readAxes) :
-// #endif
-    IndexBase(key, "radosKeyValue"),
-    location_(name, 0),
-    idx_kv_(name.uri()) {
+    // #endif
+    IndexBase(key, "radosKeyValue"), location_(name, 0), idx_kv_(name.uri()) {
 
-    if (readAxes) updateAxes();
-
+    if (readAxes) {
+        updateAxes();
+    }
 }
 
 void RadosIndex::putAxisNames(const std::string& names) {
 
     idx_kv_.put("axes", names.data(), names.length());
-
 }
 
 void RadosIndex::putAxisValue(const std::string& axis, const std::string& value) {
@@ -97,22 +116,15 @@ void RadosIndex::putAxisValue(const std::string& axis, const std::string& value)
 
     if (axis_kv == axis_kvs_.end()) {
         std::string kv_name = key().valuesToString() + std::string{"."} + axis;
-        axis_kvs_.emplace(
-            std::piecewise_construct, 
-            std::forward_as_tuple(axis), 
-            std::forward_as_tuple(
-                location_.radosName().nspace().pool().name(),
-                location_.radosName().nspace().name(),
-                kv_name
-            )
-        );
+        axis_kvs_.emplace(std::piecewise_construct, std::forward_as_tuple(axis),
+                          std::forward_as_tuple(location_.radosName().nspace().pool().name(),
+                                                location_.radosName().nspace().name(), kv_name));
 
         axis_kv = axis_kvs_.find(axis);
     }
 
     std::string v{"1"};
     axis_kv->second.put(value, v.data(), v.length());
-
 }
 
 void RadosIndex::updateAxes() {
@@ -121,7 +133,7 @@ void RadosIndex::updateAxes() {
     /// - ensure axis kv exists (daos_obj_open)
 
     int axis_names_max_len = 512;  /// @todo: take from config
-    std::vector<char> axes_data((long) axis_names_max_len);
+    std::vector<char> axes_data((long)axis_names_max_len);
 
     /// @note: performed RPCs:
     /// - get axes key size and content (daos_kv_get without buffer + daos_kv_get)
@@ -135,7 +147,8 @@ void RadosIndex::updateAxes() {
         /// @note: performed RPCs:
         /// - generate axis kv oid (daos_obj_generate_oid)
         /// - ensure axis kv exists (daos_obj_open)
-        eckit::RadosKeyValue axis_kv{idx_kv_.nspace().pool().name(), idx_kv_.nspace().name(), indexKey + std::string{"."} + name};
+        eckit::RadosKeyValue axis_kv{idx_kv_.nspace().pool().name(), idx_kv_.nspace().name(),
+                                     indexKey + std::string{"."} + name};
 
         /// @note: performed RPCs:
         /// - one or more kv list (daos_kv_list)
@@ -143,10 +156,9 @@ void RadosIndex::updateAxes() {
     }
 
     axes_.sort();
-
 }
 
-bool RadosIndex::get(const Key &key, const Key &remapKey, Field &field) const {
+bool RadosIndex::get(const Key& key, const Key& remapKey, Field& field) const {
 
     /// @note: performed RPCs:
     /// - ensure index kv exists (daos_obj_open)
@@ -154,54 +166,52 @@ bool RadosIndex::get(const Key &key, const Key &remapKey, Field &field) const {
     std::string query{key.valuesToString()};
 
     int field_loc_max_len = 512;  /// @todo: read from config
-    std::vector<char> loc_data((long) field_loc_max_len);
+    std::vector<char> loc_data((long)field_loc_max_len);
     long res;
 
     try {
 
         /// @note: performed RPCs:
         /// - retrieve field array location from index kv (daos_kv_get)
-        res = idx_kv_.get(query, &loc_data[0], (long) field_loc_max_len);
-
-    } catch (eckit::RadosEntityNotFoundException& e) {
+        res = idx_kv_.get(query, &loc_data[0], (long)field_loc_max_len);
+    }
+    catch (eckit::RadosEntityNotFoundException& e) {
 
         /// @note: performed RPCs:
         /// - close index kv (daos_obj_close)
 
         return false;
-
     }
 
-    eckit::MemoryStream ms{&loc_data[0], (size_t) res};
+    eckit::MemoryStream ms{&loc_data[0], (size_t)res};
 
     /// @note: timestamp read for informational purpoes. See note in DaosIndex::add.
     time_t ts;
     ms >> ts;
 
     fdb5::FieldLocation* loc = eckit::Reanimator<fdb5::FieldLocation>::reanimate(ms);
-    field = fdb5::Field(std::move(*loc), ts, fdb5::FieldDetails());
+    field                    = fdb5::Field(std::move(*loc), ts, fdb5::FieldDetails());
 
     /// @note: performed RPCs:
     /// - close index kv (daos_obj_close)
 
     return true;
-
 }
 
-void RadosIndex::add(const Key &key, const Field &field) {
+void RadosIndex::add(const Key& key, const Field& field) {
 
-    eckit::MemoryHandle h{(size_t) PATH_MAX};
+    eckit::MemoryHandle h{(size_t)PATH_MAX};
     eckit::HandleStream hs{h};
     h.openForWrite(eckit::Length(0));
     {
         eckit::AutoClose closer(h);
         /// @note: in the POSIX back-end, keeping a timestamp per index is necessary, to allow
-        ///   determining which was the latest indexed field in cases where multiple processes 
+        ///   determining which was the latest indexed field in cases where multiple processes
         ///   index a same field or in cases where multiple catalogues are combined with DistFDB.
         ///   In the DAOS back-end, however, determining the latest indexed field is straigthforward
-        ///   as all parallel processes writing fields for a same index key will share a DAOS 
+        ///   as all parallel processes writing fields for a same index key will share a DAOS
         ///   key-value, and the last indexing will supersede the previous ones.
-        ///   DistFDB will be obsoleted in favour of a centralised catalogue mechanism which can 
+        ///   DistFDB will be obsoleted in favour of a centralised catalogue mechanism which can
         ///   index fields on multiple catalogues.
         ///   Therefore keeping timestamps in DAOS should not be necessary.
         ///   They are kept for now only for informational purposes.
@@ -218,12 +228,11 @@ void RadosIndex::add(const Key &key, const Field &field) {
     /// - ensure index kv exists (daos_obj_open)
     /// - record field key and location into index kv (daos_kv_put)
     /// - close index kv when destroyed (daos_obj_close)
-    idx_kv_.put(key.valuesToString(), h.data(), hs.bytesWritten());   
-
+    idx_kv_.put(key.valuesToString(), h.data(), hs.bytesWritten());
 }
 
-void RadosIndex::entries(EntryVisitor &visitor) const {
-    
+void RadosIndex::entries(EntryVisitor& visitor) const {
+
     Index instantIndex(const_cast<RadosIndex*>(this));
 
     // Allow the visitor to selectively decline to visit the entries in this index
@@ -235,56 +244,56 @@ void RadosIndex::entries(EntryVisitor &visitor) const {
 
         for (const auto& key : idx_kv_.keys()) {
 
-            if (key == "axes" || key == "key") continue;
+            if (key == "axes" || key == "key")
+                continue;
 
-            /// @note: the DaosCatalogue is currently indexing a serialised DaosFieldLocation for each 
-            ///   archived field key. In the list pathway, DaosLazyFieldLocations are built for all field 
-            ///   keys present in an index -- without retrieving the actual location --, and 
+            /// @note: the DaosCatalogue is currently indexing a serialised DaosFieldLocation for each
+            ///   archived field key. In the list pathway, DaosLazyFieldLocations are built for all field
+            ///   keys present in an index -- without retrieving the actual location --, and
             ///   ListVisitor::visitDatum is called for each (see note at the top of DaosLazyFieldLocation.h).
-            ///   When a field key is matched in visitDatum, DaosLazyFieldLocation::stableLocation is called, 
-            ///   which in turn calls this method here and triggers retrieval and deserialisation of the 
-            ///   indexed DaosFieldLocation, and returns it. Since the deserialised instance is of a 
+            ///   When a field key is matched in visitDatum, DaosLazyFieldLocation::stableLocation is called,
+            ///   which in turn calls this method here and triggers retrieval and deserialisation of the
+            ///   indexed DaosFieldLocation, and returns it. Since the deserialised instance is of a
             ///   polymorphic class, it needs to be reanimated.
             fdb5::FieldLocation* loc = new fdb5::RadosLazyFieldLocation(location_.radosName(), key);
             fdb5::Field field(std::move(*loc), time_t(), fdb5::FieldDetails());
             visitor.visitDatum(field, key);
-
         }
-
     }
 }
 
-const std::vector<eckit::URI> RadosIndex::dataURIs() const {
+std::vector<eckit::URI> RadosIndex::dataURIs() const {
 
     /// @note: if daos index + daos store, this will return a uri to a DAOS array for each indexed field
     /// @note: if daos index + posix store, this will return a vector of unique uris to all referenced posix files
     ///   in this index (one for each writer process that has written to the index)
-    /// @note: in the case where we have a daos store, the current implementation of dataURIs is unnecessarily inefficient.
-    ///   This method is only called in DaosWipeVisitor, where the uris obtained from this method are processed to obtain
-    ///   unique store container paths - will always result in just one container uri! Having a URI store for each index in
-    ///   DAOS could make this process more efficient, but it would imply more KV operations and slow down field writes.
-    /// @note: in the case where we have a posix store there will be more than one unique store file paths. The current 
+    /// @note: in the case where we have a daos store, the current implementation of dataURIs is unnecessarily
+    /// inefficient.
+    ///   This method is only called in DaosWipeVisitor, where the uris obtained from this method are processed to
+    ///   obtain unique store container paths - will always result in just one container uri! Having a URI store for
+    ///   each index in DAOS could make this process more efficient, but it would imply more KV operations and slow down
+    ///   field writes.
+    /// @note: in the case where we have a posix store there will be more than one unique store file paths. The current
     ///   implementation is still inefficient but preferred to maintaining a URI store in the DAOS catalogue
 
     std::set<eckit::URI> res;
 
     for (const auto& key : idx_kv_.keys()) {
 
-        if (key == "axes" || key == "key") continue;
+        if (key == "axes" || key == "key")
+            continue;
 
         std::vector<char> data;
         eckit::MemoryStream ms = idx_kv_.getMemoryStream(data, key, "index kv");
-        
+
         time_t ts;
         ms >> ts;
 
         std::unique_ptr<fdb5::FieldLocation> fl(eckit::Reanimator<fdb5::FieldLocation>::reanimate(ms));
         res.insert(fl->uri());
-
     }
 
     return std::vector<eckit::URI>(res.begin(), res.end());
-
 }
 
 #ifdef fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH
@@ -295,10 +304,9 @@ void RadosIndex::flush() {
     }
 
     idx_kv_.flush();
-
 }
 #endif
 
 //-----------------------------------------------------------------------------
 
-} // namespace fdb5
+}  // namespace fdb5
--- src/fdb5/rados/RadosIndex.h
+++ src/fdb5/rados/RadosIndex.h
@@ -13,13 +13,22 @@
 
 #pragma once
 
-#include "eckit/io/rados/RadosNamespace.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/filesystem/URI.h"
 #include "eckit/io/rados/RadosKeyValue.h"
-#include "eckit/io/rados/RadosAsyncKeyValue.h"
+#include "eckit/io/rados/RadosNamespace.h"
 
-#include "fdb5/fdb5_config.h"
+#include "fdb5/database/EntryVisitMechanism.h"
+#include "fdb5/database/Field.h"
 #include "fdb5/database/Index.h"
+#include "fdb5/database/IndexStats.h"
+#include "fdb5/database/Key.h"
 #include "fdb5/rados/RadosIndexLocation.h"
+
+#include <map>
+#include <ostream>
+#include <string>
+#include <vector>
 
 namespace fdb5 {
 
@@ -28,30 +37,30 @@ namespace fdb5 {
 
 class RadosIndex : public IndexBase {
 
-public: // methods
+public:  // methods
 
     /// @note: creates a new index in DAOS, in the container pointed to by 'name'
     RadosIndex(const Key& key, const eckit::RadosNamespace& name);
     /// @note: used to represent and operate with an index which already exists in DAOS
-// #if defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_WRITE) || defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH)
-//     RadosIndex(const Key& key, const eckit::RadosPersistentKeyValue& name, bool readAxes = true);
-// #else
+    // #if defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_WRITE) || defined(fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH)
+    //     RadosIndex(const Key& key, const eckit::RadosPersistentKeyValue& name, bool readAxes = true);
+    // #else
     RadosIndex(const Key& key, const eckit::RadosKeyValue& name, bool readAxes = true);
-// #endif
+    // #endif
 
     void flock() const override { NOTIMP; }
     void funlock() const override { NOTIMP; }
 
-    /// @note: these methods are required for RadosCatalogueWriter to directly manipulate 
+    /// @note: these methods are required for RadosCatalogueWriter to directly manipulate
     /// idx_kv_ and axis_kvs_ within the RadosIndex. Upon flush, the index will flush all
     /// operations performed on these kvs (if PERSIST_ON_FLUSH).
     void putAxisNames(const std::string& names);
     void putAxisValue(const std::string& axis, const std::string& value);
 
-private: // methods
+private:  // methods
 
     const IndexLocation& location() const override { return location_; }
-    const std::vector<eckit::URI> dataURIs() const override;
+    std::vector<eckit::URI> dataURIs() const override;
 
     bool dirty() const override { NOTIMP; }
 
@@ -61,8 +70,8 @@ private: // methods
 
     void visit(IndexLocationVisitor& visitor) const override { NOTIMP; }
 
-    bool get( const Key &key, const Key &remapKey, Field &field ) const override;
-    void add( const Key &key, const Field &field ) override;
+    bool get(const Key& key, const Key& remapKey, Field& field) const override;
+    void add(const Key& key, const Field& field) override;
 #ifdef fdb5_HAVE_RADOS_BACKENDS_PERSIST_ON_FLUSH
     void flush() override;
 #else
@@ -71,15 +80,17 @@ private: // methods
     void encode(eckit::Stream& s, const int version) const override { NOTIMP; }
     void entries(EntryVisitor& visitor) const override;
 
-    void print( std::ostream &out ) const override { NOTIMP; }
-    void dump(std::ostream& out, const char* indent, bool simple = false, bool dumpFields = false) const override { NOTIMP; }
+    void print(std::ostream& out) const override { NOTIMP; }
+    void dump(std::ostream& out, const char* indent, bool simple = false, bool dumpFields = false) const override {
+        NOTIMP;
+    }
 
     IndexStats statistics() const override { NOTIMP; }
 
     /// @note: reads complete axis info from DAOS.
     void updateAxes();
 
-private: // members
+private:  // members
 
     fdb5::RadosIndexLocation location_;
 
@@ -90,9 +101,8 @@ private: // members
     eckit::RadosKeyValue idx_kv_;
     std::map<std::string, eckit::RadosKeyValue> axis_kvs_;
 #endif
-
 };
 
 //----------------------------------------------------------------------------------------------------------------------
 
-} // namespace fdb5
+}  // namespace fdb5
