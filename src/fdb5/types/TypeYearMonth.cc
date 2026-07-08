f502d36a7b9dcae95bdc5ffb4249a0c769b6750e
Tobias Kremer <tobias.kremer@ecmwf.int>
2026-07-08 21:48:11 +0200

    feat: Introduced YearMonth data type

---
+++ src/fdb5/types/TypeYearMonth.cc
@@ -0,0 +1,58 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/types/Date.h"
+#include "metkit/mars/MarsRequest.h"
+
+#include "fdb5/types/TypeYearMonth.h"
+#include "fdb5/types/TypesFactory.h"
+
+
+namespace fdb5 {
+
+TypeYearMonth::TypeYearMonth(const std::string& name, const std::string& type) : Type(name, type) {}
+
+TypeYearMonth::~TypeYearMonth() {}
+
+static long yearMonth(const std::string& value) {
+    eckit::Date date(value);
+    return date.year() * 100 + date.month();
+}
+
+std::string TypeYearMonth::toKey(const std::string& value) const {
+
+    return std::to_string(yearMonth(value));
+}
+
+void TypeYearMonth::getValues(const metkit::mars::MarsRequest& request, const std::string& keyword,
+                              eckit::StringList& values, const CatalogueReader*) const {
+    std::vector<eckit::Date> dates;
+
+    request.getValues(keyword, dates, true);
+
+    values.reserve(dates.size());
+
+    eckit::Translator<eckit::Date, std::string> t;
+
+    for (std::vector<eckit::Date>::const_iterator i = dates.begin(); i != dates.end(); ++i) {
+        const eckit::Date& date = *i;
+        values.push_back(t(date));
+    }
+}
+
+void TypeYearMonth::print(std::ostream& out) const {
+    out << "TypeYearMonth[name=" << name_ << "]";
+}
+
+static TypeBuilder<TypeYearMonth> type("YearMonth");
+
+
+}  // namespace fdb5
