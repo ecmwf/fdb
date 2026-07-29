5c73fcee72d0d5ce378b17398267a2306c4cd41e
Metin Cakircali <metin.cakircali@ecmwf.int>
2026-07-29 19:47:19 +0200

    fix(readlimiter): remove redundant init

--- src/fdb5/api/RemoteFDB.cc
+++ src/fdb5/api/RemoteFDB.cc
@@ -14,12 +14,10 @@
 #include "fdb5/remote/RemoteFieldLocation.h"
 #include "fdb5/remote/client/Client.h"
 #include "fdb5/remote/client/ClientConnectionRouter.h"
-#include "fdb5/remote/client/ReadLimiter.h"
 #include "fdb5/rules/Schema.h"
 
 #include "metkit/mars/MarsRequest.h"
 
-#include "eckit/config/Resource.h"
 #include "eckit/container/Queue.h"
 #include "eckit/exception/Exceptions.h"
 #include "eckit/io/Buffer.h"
@@ -242,13 +240,6 @@ RemoteFDB::RemoteFDB(const Configuration& config, const std::string& name) : Loc
     config_.set("stores", stores);
     config_.set("fieldLocationEndpoints", fieldLocationEndpoints);
     config_.overrideSchema(static_cast<std::string>(controlEndpoint()) + "/schema", schema);
-
-    /// @note: We must instantiate the ReadLimiter before any RemoteStores due to their static initialisation.
-    /// @todo: this may change in future.
-    static size_t memoryLimit =
-        Resource<size_t>("$FDB_READ_LIMIT;fdbReadLimit",
-                         config_.userConfig().getUnsigned("limits.read", size_t(1) * 1024 * 1024 * 1024));  // 1GiB
-    ReadLimiter::init(memoryLimit);
 }
 
 RemoteFDB::~RemoteFDB() {
