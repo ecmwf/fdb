0fe293c3bb76c97d309cfe170c3f456cedb1f994
Metin Çakırcalı <metin.cakircali@ecmwf.int>
2026-08-11 18:24:20 +0200

    test(remote): removed wipe

--- tests/fdb/remote/test_remote_api.cc
+++ tests/fdb/remote/test_remote_api.cc
@@ -549,16 +549,7 @@ CASE("Remote protocol: concurrent blocking control RPCs are not serialised") {
         EXPECT_EQUAL(result, 0);
     }
 
-    // wait for the reconsolidation to finish. Checking with dry-run wipe(s)
-    wipe_dry_run_stable("class=od");
-
-    // Clean up the data archived by this test so the FDB is left in a clean state.
-    eckit::Log::info() << "[CLIENT]" << "Wiping concurrent-RPC test data. --doit" << std::endl;
-    auto wipe = FDB{}.wipe(FDBToolRequest::requestsFromString("class=od")[0], true, false, true);
-    WipeElement elem;
-    while (wipe.next(elem)) {
-        eckit::Log::info() << "[CLIENT]" << elem;
-    }
+    // This is the final case; no wipe.
 }
 
 }  // namespace fdb5::test
