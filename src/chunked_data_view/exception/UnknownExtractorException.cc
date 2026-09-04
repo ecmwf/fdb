c14aae6fa6ec7e7f5d41ae562c139536ca99a046
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2026-09-04 05:44:59 +0200

    fix(z3fdb): Removed erroneous pragma once

--- src/chunked_data_view/exception/UnknownExtractorException.cc
+++ src/chunked_data_view/exception/UnknownExtractorException.cc
@@ -1,7 +1,5 @@
 // SPDX-FileCopyrightText: 2026 European Centre for Medium-Range Weather Forecasts (ECMWF)
 // SPDX-License-Identifier: Apache-2.0
-#pragma once
-
 #include "chunked_data_view/exception/UnknownExtractorException.h"
 
 namespace chunked_data_view {
